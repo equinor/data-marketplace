f1c5fd5f0d8fefb65420755f3659e420e5fc5aa2
Aron Høyer <aron@hoyer.com>
2022-03-24 14:20:29 +0100

    feat: Introduce NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import type { AppProps } from 'next/app'
 
 import "focus-visible"
 import '../styles/globals.css'
+import { NavBar } from '../components/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
