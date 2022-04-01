77cc8e951b85cd99a6117d577cfd2156daafd363
Aron Høyer <aron@hoyer.com>
2022-04-01 10:18:33 +0200

    style: Add styled components for Container and Section
    - Container sets max width and padding for page content
    - Section inherits from Container, and sets vertical margin for all but the last section in a parent

---
+++ components/Section/Section.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+import { Container } from "../Container"
+
+export const Section = styled(Container)`
+  &:not(:last-child) {
+    margin-bottom: 2rem;
+
+    @media screen and (min-width: 768px) {
+      margin-bottom: 2.5rem;
+    }
+
+    @media screen and (min-width: 992px) {
+      margin-bottom: 3.5rem;
+    }
+
+    @media screen and (min-width: 1200px) {
+      margin-bottom: 5rem;
+    }
+  }
+`
