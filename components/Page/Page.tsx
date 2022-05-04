5addf5e7aebf7fd41a96dc452866bc4ac438fbcd
Aron Høyer <aron@hoyer.com>
2022-05-04 13:17:44 +0200

    refactor: Remove superfluous endpoint.
    Access token returned from AD sign in magically authenticates us for Collibra now.
    No need for client credentials flow.

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -9,13 +9,12 @@ import { Typography } from "@equinor/eds-core-react"
 import { FunctionComponent, useEffect, useState } from "react"
 
 import { config } from "../../config"
-import { HttpClient } from "../../lib/HttpClient"
 import { NavBar } from "../NavBar"
 
 export const Page: FunctionComponent = ({ children }) => {
   const { error: msalError } = useMsalAuthentication(InteractionType.Redirect, {
     redirectUri: `${config.BASE_URL}/auth/redirect`,
-    scopes: ["https://equinor-dev.collibra.com/user_impersonation"],
+    scopes: ["openid", "https://equinor-dev.collibra.com/user_impersonation"],
   })
   const { instance, inProgress, accounts } = useMsal()
   const [loading, setLoading] = useState<boolean>(false)
@@ -33,15 +32,10 @@ export const Page: FunctionComponent = ({ children }) => {
         try {
           const tokenResponse = await instance.acquireTokenSilent({
             account: accounts[0],
-            scopes: ["https://equinor-dev.collibra.com/user_impersonation"],
+            scopes: ["openid", "https://equinor-dev.collibra.com/user_impersonation"],
           })
 
-          const res = await HttpClient.post("/api/auth/token", {
-            headers: new Headers({ "Content-Type": "application/x-www-form-urlencoded" }),
-            body: new URLSearchParams({ id_token: tokenResponse.idToken }),
-          })
-
-          localStorage.setItem("access_token", res.body.access_token)
+          localStorage.setItem("access_token", tokenResponse.accessToken)
           setLoading(false)
         } catch (error) {
           console.error("[Page] failed to acquire client access token", error)
