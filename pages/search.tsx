aada01af0eb912e30d45ba55952ec8e91f27f0a6
Aron Høyer <aron@hoyer.com>
2022-05-24 11:21:10 +0200

    feat: Add filter by community

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,19 +2,20 @@ import {
   Button,
   Card,
   Divider,
+  EdsProvider,
   Icon,
   Typography,
 } from "@equinor/eds-core-react"
 import { grid_on as gridOn, list } from "@equinor/eds-icons"
 import { tokens } from "@equinor/eds-tokens"
 import type { NextPage } from "next"
+import Link from "next/link"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 import { Container } from "../components/Container"
 import { FullPageSpinner } from "../components/FullPageSpinner/FullPageSpinner"
-import { Page } from "../components/Page"
 import { Section } from "../components/Section"
 import { HttpClient } from "../lib/HttpClient"
 
@@ -41,7 +42,7 @@ const ViewModeActionsContainer = styled.div`
   }
 `
 
-const FilterSection = styled.div`
+const FilterSection = styled(Section)`
   &:not(:last-child) {
     margin-bottom: 1.25rem;
   }
@@ -54,14 +55,23 @@ const FilterSectionHeadline = styled(Typography).attrs(() => ({ variant: "body_s
 const TagsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  grid-gap: 0.25rem;
+  grid-gap: 0.5rem;
 `
 
-const Tag = styled.span`
-  padding: 0.25rem 0.5rem calc(0.25rem + 1px);
-  border-radius: 0.25rem;
+const Tag = styled(Button)<{ active: boolean }>`
+  padding: 0 0.5rem;
   font-size: 0.75rem;
-  background-color: ${tokens.colors.ui.background__info.hex};
+  background-color: ${tokens.colors.infographic.primary__moss_green_13.rgba};
+  border: none;
+  color: ${tokens.colors.text.static_icons__default.hex};
+  ${({ active }) => active && css`
+    box-shadow: 0 0 0 2px ${tokens.colors.infographic.primary__moss_green_55.rgba};
+  `}
+
+  &:hover {
+    background-color: ${tokens.colors.infographic.primary__moss_green_34.rgba};
+    border: none;
+  }
 `
 
 const SearchResultsList = styled.ul`
@@ -70,7 +80,7 @@ const SearchResultsList = styled.ul`
   list-style: none;
 `
 
-const SearchResultCard = styled(Card).attrs(() => ({ as: "li" }))`
+const SearchResultCard = styled.li`
   box-shadow: ${tokens.elevation.raised};
   border-radius: ${tokens.shape.corners.borderRadius};
 
@@ -85,9 +95,27 @@ const MetaContainer = styled.div`
 
 const Search: NextPage = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [communities, setCommunities] = useState<any[]>([])
   const [searchResults, setSearchResults] = useState<any[]>([])
 
   const router = useRouter()
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { body } = await HttpClient.get("/api/collibra/communities", {
+          headers: { authorization: `Bearer ${localStorage.getItem("access_token")}` },
+          query: {
+            // TODO: get this ID from Collibra
+            parentId: "1f63fe17-a732-4779-a290-db088803761c",
+          },
+        })
+        setCommunities(body.results)
+      } catch (error) {
+        console.error("[Search] Failed fetching communities", error)
+      }
+    })()
+  }, [])
 
   useEffect(() => {
     setIsLoading(true)
@@ -95,106 +123,145 @@ const Search: NextPage = () => {
     if (router.query.q) {
       (async () => {
         try {
-          const { body } = await HttpClient.post("/api/collibra/search", {
-            body: {
-              keywords: router.query.q,
-            },
+          const { body } = await HttpClient.get("/api/search", {
+            headers: { authorization: `Bearer ${localStorage.getItem("access_token")}` },
+            query: router.query,
           })
 
           setSearchResults(body.results.map((result: any) => result.resource))
+          setIsLoading(false)
         } catch (error) {
           console.error("[Search] Failed fetching search results", error)
+          setIsLoading(false)
         }
       })()
     }
-
-    setIsLoading(false)
   }, [router])
+
+  const onCommunityFilterClick = (id: string) => {
+    let { community } = router.query
+
+    if (!community) {
+      community = id
+    } else if (typeof community === "string") {
+      if (community === id) {
+        community = undefined
+      } else {
+        community = [community, id]
+      }
+    } else {
+      const existingIdx = community.findIndex((f) => f === id)
+      if (existingIdx === -1) {
+        community.push(id)
+      } else {
+        community = [
+          ...community.slice(0, existingIdx),
+          ...community.slice(existingIdx + 1, community.length),
+        ]
+      }
+    }
+
+    router.push({
+      pathname: router.pathname,
+      query: { ...router.query, community },
+    })
+  }
 
   return (
     <FullPageSpinner show={isLoading}>
-      <Page>
-        <SearchPageContainer>
-          <aside>
-            <Typography variant="h5" as="p">Filter</Typography>
-            <Divider variant="small" />
+      <SearchPageContainer>
+        <aside>
+          <Typography variant="h5" as="p">Filter</Typography>
+          <Divider variant="small" />
 
-            <FilterSection>
-              <FilterSectionHeadline>Communities</FilterSectionHeadline>
+          <FilterSection>
+            <FilterSectionHeadline>Communities</FilterSectionHeadline>
 
-              <TagsContainer>
-                {Array.from({ length: 5 }).map((_community, i) => (
-                  <Tag key={`community-${i + 1}`}>
-                    Community
-                    {" "}
-                    {i + 1}
+            <TagsContainer>
+              <EdsProvider density="compact">
+                {communities.map((community) => (
+                  <Tag
+                    active={!!router.query.community?.includes(community.id)}
+                    key={community.id}
+                    onClick={() => onCommunityFilterClick(community.id)}
+                  >
+                    {community.name}
                   </Tag>
                 ))}
-              </TagsContainer>
-            </FilterSection>
-          </aside>
+              </EdsProvider>
+            </TagsContainer>
+          </FilterSection>
+        </aside>
 
-          <main>
-            <Section>
-              {searchResults.length > 0 ? (
-                <>
-                  <SearchResultsHeader>
-                    <Typography variant="body_short">
-                      {searchResults.length}
-                      {" "}
-                      Results
-                    </Typography>
+        <main>
+          <Section>
+            {searchResults.length > 0 ? (
+              <>
+                <SearchResultsHeader>
+                  <Typography variant="body_short">
+                    {searchResults.length}
+                    {" "}
+                    Results
+                  </Typography>
 
-                    <ViewModeActionsContainer>
-                      <Typography variant="body_short">View</Typography>
+                  <ViewModeActionsContainer>
+                    <Typography variant="body_short">View</Typography>
 
-                      <Button variant="ghost_icon" color="secondary">
-                        <Icon data={list} />
-                      </Button>
+                    <Button variant="ghost_icon" color="secondary">
+                      <Icon data={list} />
+                    </Button>
 
-                      <Button variant="ghost_icon" color="secondary">
-                        <Icon data={gridOn} />
-                      </Button>
-                    </ViewModeActionsContainer>
-                  </SearchResultsHeader>
+                    <Button variant="ghost_icon" color="secondary">
+                      <Icon data={gridOn} />
+                    </Button>
+                  </ViewModeActionsContainer>
+                </SearchResultsHeader>
 
-                  <SearchResultsList>
-                    {searchResults.map((resource) => (
-                      <SearchResultCard key={resource.id}>
-                        <Card.Header>
-                          <Typography variant="h4" as="p">{resource.displayName}</Typography>
-                        </Card.Header>
+                <SearchResultsList>
+                  {searchResults.map((resource) => (
+                    <SearchResultCard key={resource.id}>
+                      <Link href={{ pathname: "/assets/[assetId]", query: { assetId: resource.id } }}>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a>
+                          <Card.Header>
+                            <Typography variant="h4" as="p">{resource.displayName}</Typography>
+                          </Card.Header>
 
-                        <Card.Content>
-                          <MetaContainer>
-                            <Typography variant="caption">
-                              Last updated on
-                              {" "}
-                              {Intl.DateTimeFormat("nb").format(new Date(resource.lastModifiedOn))}
-                            </Typography>
-                          </MetaContainer>
+                          <Card.Content>
+                            <MetaContainer>
+                              <Typography variant="caption">
+                                Last updated on
+                                {" "}
+                                {Intl.DateTimeFormat("nb").format(new Date(resource.lastModifiedOn))}
+                              </Typography>
+                            </MetaContainer>
 
-                          <Typography variant="body_short">Description</Typography>
-                        </Card.Content>
-                      </SearchResultCard>
-                    ))}
-                  </SearchResultsList>
-                </>
-              ) : (
-                <Typography>
-                  We were not able to find anything related to
-                  {" "}
-                  <b>
-                    &quot;
-                    {router.query.q}
-                    &quot;
-                  </b>
-                </Typography>
-              )}
-            </Section>
-          </main>
-        </SearchPageContainer>
-      </Page>
+                            {/*
+                              * html strings are sanitized in backend at this point,
+                              * so we should(TM) be safe to do this
+                              */}
+                            <Typography variant="body_short" dangerouslySetInnerHTML={{ __html: resource.description }} />
+                          </Card.Content>
+                        </a>
+                      </Link>
+                    </SearchResultCard>
+                  ))}
+                </SearchResultsList>
+              </>
+            ) : (
+              <Typography>
+                We were not able to find anything related to
+                {" "}
+                <b>
+                  &quot;
+                  {router.query.q}
+                  &quot;
+                </b>
+              </Typography>
+            )}
+          </Section>
+        </main>
+      </SearchPageContainer>
     </FullPageSpinner>
   )
 }
