99cb4729aab6f4a9211443a024b47f8f790498a2
Aron Høyer <aron@hoyer.com>
2022-06-02 15:13:08 +0200

    feat: Add backend support for getting asset responsibilities

---
+++ pages/api/assets/[id]/responsibilities.ts
@@ -0,0 +1,48 @@
+import { NextApiHandler } from "next"
+
+import { config } from "../../../../config"
+import { HttpClient } from "../../../../lib/HttpClient"
+
+const AssetResponsibilitiesHandler: NextApiHandler = async (req, res) => {
+  if (req.method !== "GET") {
+    res.status(405).end()
+  } else {
+    try {
+      const response = await HttpClient.get(`${config.COLLIBRA_BASE_URL}/responsibilities`, {
+        headers: { authorization: req.headers.authorization },
+        query: { resourceIds: req.query.id },
+      })
+
+      // filter out results that are not a "Role" and "User" type
+      const usersWithRoles = response.body.results.filter((result: any) => (
+        result.role.resourceType === "Role"
+        && result.owner.resourceType === "User"
+      )).map((responsibility: any) => ({
+        role: responsibility.role.name.toUpperCase().replace(/\s/g, "_"),
+        id: responsibility.owner.id,
+      }))
+
+      const usersRes = await Promise.all(usersWithRoles.map((user: any) => HttpClient.get(`${config.COLLIBRA_BASE_URL}/users/${user.id}`, {
+        headers: { authorization: req.headers.authorization },
+      })))
+
+      const users = usersWithRoles.map((user: any) => {
+        const fullUser = usersRes.find((r) => r.body.id === user.id)?.body
+
+        return {
+          ...user,
+          firstName: fullUser.firstName,
+          lastName: fullUser.lastName,
+          email: fullUser.emailAddress,
+        }
+      })
+
+      res.json(users)
+    } catch (error) {
+      console.log("[AssetResponsibilitiesHandler]", error)
+      res.status((error as any).statusCode ?? 500).end()
+    }
+  }
+}
+
+export default AssetResponsibilitiesHandler
--- pages/assets/[id].tsx
+++ pages/assets/[id].tsx
@@ -1,19 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Typography } from "@equinor/eds-core-react"
 import type { NextPage } from "next"
+import Link from "next/link"
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 import { Container } from "../../components/Container"
 import { HttpClient } from "../../lib/HttpClient"
 
+const mapHashToApiEndpoint = (assetId: string, hash: string) => ({
+  "#overview": `/api/assets/${assetId}`,
+  "#responsibilities": `/api/assets/${assetId}/responsibilities`,
+})[hash]
+
 const AssetDetailView: NextPage = () => {
+  const [currentTab, setCurrentTab] = useState<string>("")
+
   const router = useRouter()
 
   useEffect(() => {
     if (router.query.id) {
+      setCurrentTab(window.location.hash)
+    }
+  }, [router])
+
+  useEffect(() => {
+    if (currentTab && router.query.id) {
+      const endpoint = mapHashToApiEndpoint(router.query.id as string, currentTab);
+
       (async () => {
         try {
-          const res = await HttpClient.get(`/api/assets/${router.query.id}`, {
+          const res = await HttpClient.get(endpoint!, {
             headers: { authorization: `Bearer ${window.localStorage.getItem("access_token")}` },
           })
           console.log(res)
@@ -22,11 +39,47 @@ const AssetDetailView: NextPage = () => {
         }
       })()
     }
-  }, [router])
+  }, [currentTab, router])
+
+  // useEffect(() => {
+  //   if (router.query.id) {
+  //     (async () => {
+  //       try {
+  //         const res = await HttpClient.get(`/api/assets/${router.query.id}`, {
+  //           headers: { authorization: `Bearer ${window.localStorage.getItem("access_token")}` },
+  //         })
+  //         console.log(res)
+  //       } catch (error) {
+  //         console.error("[AssetDetailView] Failed while getting asset", router.query.id)
+  //       }
+  //     })()
+  //   }
+  // }, [router])
 
   return (
     <main>
       <Container>
+        <aside>
+          <nav>
+            <ul>
+              <li>
+                <Link href="#overview">
+                  <a>
+                    Overview
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#responsibilities">
+                  <a>
+                    Responsibilities
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+
         <Typography variant="h1_bold">Hello, World!</Typography>
       </Container>
     </main>
