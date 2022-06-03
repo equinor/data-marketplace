9c570bc52267027b24ad1e7500afd871f6fdfc24
Aron Høyer <aron@hoyer.com>
2022-06-03 11:57:47 +0200

    feat: Add tab UI and logic

--- pages/assets/[id].tsx
+++ pages/assets/[id].tsx
@@ -1,36 +1,103 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Typography } from "@equinor/eds-core-react"
+import {
+  Button, Divider, Typography,
+} from "@equinor/eds-core-react"
+import { tokens } from "@equinor/eds-tokens"
 import type { NextPage } from "next"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import styled from "styled-components"
 
 import { Container } from "../../components/Container"
 import { HttpClient } from "../../lib/HttpClient"
 
-const mapHashToApiEndpoint = (assetId: string, hash: string) => ({
-  "#overview": `/api/assets/${assetId}/overview`,
-  "#responsibilities": `/api/assets/${assetId}/responsibilities`,
-})[hash]
+const Header = styled.header`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 1.5rem;
+  align-items: baseline;
+`
+
+const ContentContainer = styled.div`
+  display: grid;
+  grid-template-columns: minmax(10rem, 13rem) 1fr;
+  grid-gap: 1.5rem;
+`
+
+const TabNavContainer = styled.aside`
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+
+    li {
+      position: relative;
+      left: -0.75rem;
+    }
+  }
+`
+
+const TabLink = styled.a<{ isActive: boolean }>`
+  padding: 0.5rem 0.75rem calc(0.5rem + 1px);
+  display: inline-block;
+  border-radius: ${tokens.shape.button.borderRadius};
+  background-color: ${({ isActive }) => (isActive ? tokens.colors.interactive.secondary__highlight.hex : "transparent")};
+
+  &:hover {
+    background-color: ${tokens.colors.interactive.secondary__highlight.hex};
+  }
+`
+
+const tabs = [
+  {
+    getDataSrc: (assetID: string) => `/api/assets/${assetID}/overview`,
+    hash: "#overview",
+    key: "overview",
+    label: "Overview",
+  },
+  {
+    getDataSrc: (assetID: string) => `/api/assets/${assetID}/responsibilities`,
+    hash: "#responsibilities",
+    key: "responsibilities",
+    label: "Responsibilities",
+  },
+]
 
 const AssetDetailView: NextPage = () => {
-  const [currentTab, setCurrentTab] = useState<string>("")
+  const [currentTab, setCurrentTab] = useState<typeof tabs[0]>()
+  const [assetData, setAssetData] = useState<any>()
 
   const router = useRouter()
 
   useEffect(() => {
     if (router.query.id) {
-      setCurrentTab(window.location.hash)
+      if (!window.location.hash) {
+        router.replace("#overview")
+      }
+
+      if (!assetData) {
+        (async () => {
+          try {
+            const res = await HttpClient.get(`/api/assets/${router.query.id}`, {
+              headers: { authorization: `Bearer ${window.localStorage.getItem("access_token")}` },
+            })
+            setAssetData(res.body)
+          } catch (error) {
+            console.error(`[AssetDetailView] Failed while getting asset ${router.query.id}`, error)
+          }
+        })()
+      }
+
+      setCurrentTab(tabs.find((tab) => tab.hash === window.location.hash))
     }
-  }, [router])
+  }, [assetData, router])
 
   useEffect(() => {
     if (currentTab && router.query.id) {
-      const endpoint = mapHashToApiEndpoint(router.query.id as string, currentTab);
-
       (async () => {
         try {
-          const res = await HttpClient.get(endpoint!, {
+          const res = await HttpClient.get(currentTab.getDataSrc(router.query.id as string), {
             headers: { authorization: `Bearer ${window.localStorage.getItem("access_token")}` },
           })
           console.log(res)
@@ -44,28 +111,37 @@ const AssetDetailView: NextPage = () => {
   return (
     <main>
       <Container>
-        <aside>
-          <nav>
-            <ul>
-              <li>
-                <Link href="#overview">
-                  <a>
-                    Overview
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href="#responsibilities">
-                  <a>
-                    Responsibilities
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </aside>
+        <Header>
+          {assetData && (
+            <>
+              <Typography variant="h1_bold">
+                {assetData.name}
+              </Typography>
 
-        <Typography variant="h1_bold">Hello, World!</Typography>
+              <Button>Add to cart</Button>
+            </>
+          )}
+        </Header>
+
+        <Divider />
+
+        <ContentContainer>
+          <TabNavContainer>
+            <nav>
+              <ul>
+                {tabs.map((tab) => (
+                  <li key={tab.key}>
+                    <Link href={tab.hash} passHref>
+                      <TabLink isActive={currentTab?.hash === tab.hash}>
+                        {tab.label}
+                      </TabLink>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </TabNavContainer>
+        </ContentContainer>
       </Container>
     </main>
   )
