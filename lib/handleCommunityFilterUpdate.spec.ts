541314a02527fd1a8cd427cf359007cc88328739
Aron Høyer <aron@hoyer.com>
2022-06-08 14:12:43 +0200

    test: Add test for community filter update

---
+++ lib/handleCommunityFilterUpdate.spec.ts
@@ -0,0 +1,29 @@
+import { handleCommunityFilterUpdate } from "./handleCommunityFilterUpdate"
+
+describe("handleCommunityFilterUpdate", () => {
+  test("should return given ID when provided with undefined query", () => {
+    const id = "abc123"
+    expect(handleCommunityFilterUpdate(id)).toBe(id)
+  })
+
+  test("should return array when adding id to single query param", () => {
+    const id = "def123"
+    const q = "abc123"
+    const r = handleCommunityFilterUpdate(id, q) as string[]
+
+    expect(r).toBeInstanceOf(Array)
+    expect(r[0]).toBe(q)
+    expect(r[1]).toBe(id)
+  })
+
+  test("should return array containing two IDs when adding ID to query param array", () => {
+    const id = "ghi123"
+    const q = ["abc123", "def123"]
+    const r = handleCommunityFilterUpdate(id, q) as string[]
+
+    expect(r).toBeInstanceOf(Array)
+    expect(r[0]).toBe(q[0])
+    expect(r[1]).toBe(q[1])
+    expect(r[2]).toBe(id)
+  })
+})
