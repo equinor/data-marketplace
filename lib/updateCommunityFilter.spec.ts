377e744afdc8d978357ac61bf20b70400427fb07
Aron Høyer <aron@hoyer.com>
2022-06-08 15:43:40 +0200

    test: Add test cases for removal of asset ID

--- lib/updateCommunityFilter.spec.ts
+++ lib/updateCommunityFilter.spec.ts
@@ -3,27 +3,43 @@ import { updateCommunityFilter } from "./updateCommunityFilter"
 describe("handleCommunityFilterUpdate", () => {
   test("should return given ID when provided with undefined query", () => {
     const id = "abc123"
+
     expect(updateCommunityFilter(id)).toBe(id)
   })
 
   test("should return array when adding id to single query param", () => {
     const id = "def123"
     const q = "abc123"
-    const r = updateCommunityFilter(id, q) as string[]
+    const r = updateCommunityFilter(id, q)
 
     expect(r).toBeInstanceOf(Array)
-    expect(r[0]).toBe(q)
-    expect(r[1]).toBe(id)
+    expect(r!.includes(q)).toBe(true)
+    expect(r!.includes(id)).toBe(true)
   })
 
   test("should return array containing two IDs when adding ID to query param array", () => {
     const id = "ghi123"
     const q = ["abc123", "def123"]
-    const r = updateCommunityFilter(id, q) as string[]
+    const r = updateCommunityFilter(id, q)
 
     expect(r).toBeInstanceOf(Array)
-    expect(r[0]).toBe(q[0])
-    expect(r[1]).toBe(q[1])
-    expect(r[2]).toBe(id)
+    expect(r!.includes(q[0])).toBe(true)
+    expect(r!.includes(q[1])).toBe(true)
+    expect(r!.includes(id)).toBe(true)
+  })
+
+  test("should return undefined when removing ID matching single string query param", () => {
+    const id = "abc123"
+
+    expect(typeof updateCommunityFilter(id, id)).toBe("undefined")
+  })
+
+  test("should remove ID from query param array", () => {
+    const id = "def123"
+    const q = ["abc123", "def123"]
+    const r = updateCommunityFilter(id, q)
+
+    expect(r!.includes(q[0])).toBe(true)
+    expect(r!.includes(id)).toBe(false)
   })
 })
