ef7cde3b0e66da841324a40867716d896db4ac29
Aron Høyer <aron@hoyer.com>
2022-06-08 14:18:56 +0200

    docs: Add js doc to community filter update function

--- lib/handleCommunityFilterUpdate.ts
+++ lib/handleCommunityFilterUpdate.ts
@@ -1,3 +1,14 @@
+/**
+ * Appends a given community ID to the given filters, if any.
+ * If no existing filters are provided, it will simply return the given community ID.
+ * If the existing filters are a string, the function will return the updated filters as
+ * an array of strings.
+ * If the existing filters are an array, the given community ID will be pushed to the filters array.
+ * If the given ID is in the filters, it will be removed.
+ * @param id The community ID
+ * @param communities The existing filters
+ * @returns The updated query parameter with the given community ID added
+ */
 export const handleCommunityFilterUpdate = (id: string, communities?: string | string[]) => {
   let filters = communities
 
