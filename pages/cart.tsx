e8ff86410a676aea1fe64616721d64942c3f94c5
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-07-27 11:06:24 +0200

    🚸 Update cart header title

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -5,6 +5,8 @@ import {
 import {
   // eslint-disable-next-line camelcase
   chevron_right,
+  // eslint-disable-next-line camelcase
+  shopping_card,
 } from "@equinor/eds-icons"
 import { tokens } from "@equinor/eds-tokens"
 import { NextPage } from "next"
@@ -16,7 +18,7 @@ import { Container } from "../components/Container"
 import { TruncatedDescription } from "../components/helpers"
 
 const { Item } = List
-const { Header: CardHeader, HeaderTitle, Content: CardContent } = Card
+const { Header: CardHeader, HeaderTitle: CardHeaderTitle, Content: CardContent } = Card
 
 const CartItems = styled(List)`
   margin: 1.5rem 0;
@@ -42,28 +44,36 @@ const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
 `
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 0.5rem;
+`
+const CartIcon = styled(Icon)`
+  flex-shrink: 0;
+`
 
-const mockData = [
-  {
-    id: 1,
-    name: "Automatic Identification System (AIS) vessel positions",
-    // Will containt unknown, random HTML :/
-    description: `Automatic identification system (AIS) data from the AIS transcender at 
-    the offshore subsation in the Dudgeon wind farm. The AIS data is available as a a real-time
-    stream of raw data, and includes information about vessels such as unique identification,
-    position, course and speed. Automatic identification system (AIS) data from the AIS transcender at 
-    the offshore subsation in the Dudgeon wind farm. The AIS data is available as a a real-time
-    stream of raw data, and includes information about vessels such as unique identification,
-    position, course and speed.`,
-    // Dunno anything about this yet
-    domain: ["Renewables"],
-  },
-  {
-    id: 2,
-    name: "Operational timeseries measurements (raw) - wind farm",
-    description: "Raw operational timeseries data exported from the wind farm management system (Bazefield) via the Bazefield2Omnia pipeline and stored as json files in omnia data lake. Ca. 350 GB of data per day representing the tags/measurements.",
-    domain: ["Renewables", "Something else"],
-  },
+const mockData = [{
+  id: 1,
+  name: "Automatic Identification System (AIS) vessel positions",
+  // Will containt unknown, random HTML :/
+  description: `Automatic identification system (AIS) data from the AIS transcender at 
+  the offshore subsation in the Dudgeon wind farm. The AIS data is available as a a real-time
+  stream of raw data, and includes information about vessels such as unique identification,
+  position, course and speed. Automatic identification system (AIS) data from the AIS transcender at 
+  the offshore subsation in the Dudgeon wind farm. The AIS data is available as a a real-time
+  stream of raw data, and includes information about vessels such as unique identification,
+  position, course and speed.`,
+  // Dunno anything about this yet
+  domain: ["Renewables"],
+},
+{
+  id: 2,
+  name: "Operational timeseries measurements (raw) - wind farm",
+  description: "Raw operational timeseries data exported from the wind farm management system (Bazefield) via the Bazefield2Omnia pipeline and stored as json files in omnia data lake. Ca. 350 GB of data per day representing the tags/measurements.",
+  domain: ["Renewables", "Something else"],
+},
+
 ]
 
 const CartView : NextPage = () => {
@@ -72,19 +82,30 @@ const CartView : NextPage = () => {
   return (
     <Container>
       <Content>
-        <Typography variant="h1">Cart header</Typography>
+        <Title>
+          {/* eslint-disable-next-line camelcase */}
+          <CartIcon data={shopping_card} />
+          <Typography variant="h1">
+            {intl.formatMessage({ id: "cart.headline" }, {
+              count: mockData.length,
+            })}
+
+          </Typography>
+        </Title>
         <CartItems>
           {mockData.map((item) => (
             <CartItem key={item.id}>
               <Card elevation="raised">
                 <CardHeader>
-                  <HeaderTitle>
+                  <CardHeaderTitle>
                     {/* This is just a dummy example */}
                     <Tags>
                       {item.domain.map((domain) => <Chip key={domain} style={{ display: "inline-block" }}>{domain}</Chip>)}
                     </Tags>
+
                     <Typography variant="h2">{item.name}</Typography>
-                  </HeaderTitle>
+
+                  </CardHeaderTitle>
                 </CardHeader>
                 <CardContent>
                   <TruncatedDescription variant="body_long" lines={3}>{item.description}</TruncatedDescription>
