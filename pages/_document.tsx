c18cc3b6644f0c132165dad24bc0d2cbd54d6f2c
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-08-03 11:18:46 +0200

    ✨ Add Equinor favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,8 @@ const Document: VoidFunctionComponent<DocumentProps> = () => (
   <Html>
     <Head>
       <link rel="stylesheet" href="https://eds-static.equinor.com/font/equinor-font.css" />
+      <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
     </Head>
     <body>
       <Main />
