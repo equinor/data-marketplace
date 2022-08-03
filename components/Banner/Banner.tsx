268084340592d6fe2c2601537432c05d8e0bc1fc
Shikha Mishra <shikhamishra.jec@gmail.com>
2022-08-04 00:36:31 +0200

    Added EDS color tokens

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import { Icon, Typography } from "@equinor/eds-core-react"
 import { warning_filled } from "@equinor/eds-icons"
+import { tokens } from "@equinor/eds-tokens"
 import { FunctionComponent, ReactNode } from "react"
 import styled from "styled-components"
 
@@ -10,30 +11,23 @@ type Props = {
 }
 
 const BannerContainer = styled.div<{ variant: string }>`
-background-color: ${({ variant }) => (variant === "danger" ? "#FFC1C1"
-    : "#FFE7D6")};
+    background-color: ${({ variant }) => (variant === "danger" ? tokens.colors.ui.background__danger.rgba : tokens.colors.ui.background__warning.rgba)};
     display: flex;
     justify-content: start;
     gap: 15px;
-
 `
 
-const BannerIcon = styled.div<{ variant: string }>`
-color: ${({ variant }) => (variant === "danger" ? "red"
-    : "brown")};
+const BannerIcon = styled.div`
     padding-left: 5px;
-
 `
 
 export const Banner: FunctionComponent<Props> = ({ variant, children }) => (
-  <div>
-    <BannerContainer variant={variant}>
-      <BannerIcon variant={variant}>
-        <Icon data={warning_filled} />
-      </BannerIcon>
-      <Typography>
-        {children}
-      </Typography>
-    </BannerContainer>
-  </div>
+  <BannerContainer variant={variant}>
+    <BannerIcon>
+      <Icon color={variant === "danger" ? tokens.colors.interactive.danger__text.rgba : tokens.colors.interactive.warning__text.rgba} data={warning_filled} />
+    </BannerIcon>
+    <Typography>
+      {children}
+    </Typography>
+  </BannerContainer>
 )
