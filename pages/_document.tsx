eb59d7038210172d28d629ce67b49c97e247950b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-08-04 11:14:40 +0200

    👽 Remove deprected VoidFunctionComponent
    In react 18 one has to type children expliciit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,9 @@ import {
   Main,
   NextScript,
 } from "next/document"
-import type { VoidFunctionComponent } from "react"
+import { FunctionComponent } from "react"
 
-const Document: VoidFunctionComponent<DocumentProps> = () => (
+const Document: FunctionComponent<DocumentProps> = () => (
   <Html>
     <Head>
       <link rel="stylesheet" href="https://eds-static.equinor.com/font/equinor-font.css" />
