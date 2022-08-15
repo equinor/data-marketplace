5b474ca9c05c3a422c58b3c04416fc43e37c34b0
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-08-15 11:26:59 +0200

    🏷️ Use overview and responsibilities types

--- components/AssetTabContent/index.ts
+++ components/AssetTabContent/index.ts
@@ -1,2 +1,5 @@
 export { OverviewContent } from "./OverviewContent"
 export { ResponsibilitiesContent } from "./ResponsibilitiesContent"
+
+export type { OverviewContentSections } from "./OverviewContent"
+export type { ResponsibilitiesContentSections } from "./ResponsibilitiesContent"
