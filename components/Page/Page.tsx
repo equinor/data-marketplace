643b788688a87b660b4db70adb65f081a449cc26
Aron Høyer <aron@hoyer.com>
2022-08-15 14:33:59 +0200

    chore: Remove msal

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -1,75 +1,14 @@
-import { InteractionStatus, InteractionType } from "@azure/msal-browser"
-import {
-  AuthenticatedTemplate,
-  UnauthenticatedTemplate,
-  useMsal,
-  useMsalAuthentication,
-} from "@azure/msal-react"
-import { Typography } from "@equinor/eds-core-react"
-import {
-  FunctionComponent, useEffect, useState, ReactNode,
-} from "react"
-import { FormattedMessage } from "react-intl"
+import { FunctionComponent, ReactNode } from "react"
 
-import { config } from "../../config"
 import { NavBar } from "../NavBar"
 
 type Props = {
   children: ReactNode
 };
 
-export const Page: FunctionComponent<Props> = ({ children }) => {
-  const { error: msalError } = useMsalAuthentication(InteractionType.Redirect, {
-    redirectUri: `${config.BASE_URL}`,
-    scopes: ["openid", "https://equinor-dev.collibra.com/user_impersonation"],
-  })
-  const { instance, inProgress, accounts } = useMsal()
-  const [loading, setLoading] = useState<boolean>(false)
-
-  useEffect(() => {
-    if (
-      accounts.length > 0
-      && inProgress === InteractionStatus.None
-      && !msalError
-    ) {
-      setLoading(true);
-
-      (async () => {
-        try {
-          const tokenResponse = await instance.acquireTokenSilent({
-            account: accounts[0],
-            scopes: ["openid", "https://equinor-dev.collibra.com/user_impersonation"],
-          })
-
-          localStorage.setItem("access_token", tokenResponse.accessToken)
-          setLoading(false)
-        } catch (error) {
-          console.error("[Page] Failed to acquire access token. Attempting login redirect", error)
-          await instance.acquireTokenRedirect({
-            account: accounts[0],
-            scopes: ["openid", "https://equinor-dev.collibra.com/user_impersonation"],
-          })
-        }
-      })()
-    }
-  }, [instance, accounts, inProgress, msalError])
-
-  return (
-    <>
-      <AuthenticatedTemplate>
-        {loading ? (
-          <Typography><FormattedMessage id="page.authenticatedLoading" /></Typography>
-        ) : (
-          <>
-            <NavBar />
-            {children}
-          </>
-        )}
-      </AuthenticatedTemplate>
-
-      <UnauthenticatedTemplate>
-        <Typography><FormattedMessage id="page.unauthenticated" /></Typography>
-      </UnauthenticatedTemplate>
-    </>
-  )
-}
+export const Page: FunctionComponent<Props> = ({ children }) => (
+  <>
+    <NavBar />
+    {children}
+  </>
+)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,3 @@
-import { PublicClientApplication, LogLevel } from "@azure/msal-browser"
-import { MsalProvider } from "@azure/msal-react"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import { IntlProvider } from "react-intl"
@@ -8,65 +6,21 @@ import { Provider } from "react-redux"
 import "../styles/globals.css"
 
 import { Page } from "../components/Page"
-import { config } from "../config"
 import englishTexts from "../locales/english.json"
 import { store } from "../store"
 
-const msalInstance = new PublicClientApplication({
-  auth: {
-    clientId: config.AUTH_CLIENT_ID,
-    authority: config.AUTH_AUTHORITY,
-  },
-  cache: {
-    cacheLocation: "localStorage",
-    storeAuthStateInCookie: false,
-    secureCookies: false,
-  },
-  system: {
-    loggerOptions: {
-      loggerCallback: (
-        level: LogLevel,
-        message: string,
-        containsPii: boolean,
-      ): void => {
-        if (containsPii) return
-
-        switch (level) {
-          case LogLevel.Error:
-            console.error(message)
-            break
-          case LogLevel.Info:
-            console.info(message)
-            break
-          case LogLevel.Verbose:
-            console.debug(message)
-            break
-          case LogLevel.Warning:
-            console.warn(message)
-            break
-          default:
-            console.log(message)
-            break
-        }
-      },
-    },
-  },
-})
-
 const App = ({ Component, pageProps }: AppProps) => (
-  <MsalProvider instance={msalInstance}>
-    <IntlProvider locale="en" defaultLocale="en" messages={englishTexts}>
-      <Provider store={store}>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <Page>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...pageProps} />
-        </Page>
-      </Provider>
-    </IntlProvider>
-  </MsalProvider>
+  <IntlProvider locale="en" defaultLocale="en" messages={englishTexts}>
+    <Provider store={store}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Page>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </Page>
+    </Provider>
+  </IntlProvider>
 )
 
 export default App
