bd529e5c52f33d27da10ea9737eddb6fa1e5d14c
Shikha Mishra <shikhamishra.jec@gmail.com>
2022-08-19 15:36:37 +0200

    Removed all styling

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -7,7 +7,6 @@ import {
   Typography,
 } from "@equinor/eds-core-react"
 import { account_circle } from "@equinor/eds-icons"
-import { tokens } from "@equinor/eds-tokens"
 import NextLink from "next/link"
 import { useRouter } from "next/router"
 import {
@@ -19,56 +18,13 @@ import {
 import { FormattedMessage, useIntl } from "react-intl"
 import styled from "styled-components"
 
-import { Container } from "../Container"
-
-import { Cart } from "./Cart"
-
-const UserNavbar = styled.nav`
-  display: flex;
-  align-items: center;
-  padding: 0.5rem 0;
-  width: 100%;
-  background-color: ${tokens.colors.ui.background__default.hex};
+const EDSTopBar = styled(TopBar)`
+  margin-bottom: 5rem;
 `
 
-const HeaderContentContainer = styled(Container)`
-  display: flex;
-  align-items: center;
-
-  > *:not(:last-child) {
-    margin-right: 5rem;
-  }
-`
-const TopBarContainer = styled(TopBar)`
-  width: 100%;
-  padding-bottom: 4rem;  
-
-`
-const LogoContainer = styled.div`
-  width: fit-content;
-  white-space: nowrap;
-`
-
-const ActionsContainer = styled(TopBar.Actions)`
-  width: 100%;
-  background-color: ${tokens.colors.ui.background__default.hex};
-  display: flex;
-  justify-content: flex-end;
-
-`
-
-const SearchContainer = styled(TopBar.CustomContent)`
-  width: 80%;
-  background-color: ${tokens.colors.ui.background__default.hex};
-  display: flex;
-  justify-content: flex-end;
-
-`
-
-const SearchForm = styled.form`
-  width: 100%;
-  align: right;
-  margin-left: 5rem;
+const EDSCustomContent = styled(TopBar.CustomContent)`
+  width: 50%;
+  margin-left: 8rem;
 `
 
 export const NavBar = () => {
@@ -97,29 +53,27 @@ export const NavBar = () => {
   }
 
   return (
-    <HeaderContentContainer>
-      <TopBarContainer>
-        <LogoContainer>
-          <NextLink href="/" passHref>
-            <Button as="a" variant="ghost" color="secondary">
-              <Typography><FormattedMessage id="navbar.logo" /></Typography>
-            </Button>
-          </NextLink>
-        </LogoContainer>
-        <SearchContainer>
-          <SearchForm onSubmit={onSearchSubmit}>
-            <Search aria-label="sitewide" id="search-normal" placeholder={intl.formatMessage({ id: "navbar.placeholderSearch" })} onChange={onSearchChange} value={searchQuery} />
-          </SearchForm>
-        </SearchContainer>
-        <ActionsContainer>
-          <UserNavbar aria-label={intl.formatMessage({ id: "navbar.ariaUserMenu" })}>
-            <Cart />
-            <Button variant="ghost_icon" color="secondary">
-              <Icon data={account_circle} title={intl.formatMessage({ id: "navbar.titleAccount" })} />
-            </Button>
-          </UserNavbar>
-        </ActionsContainer>
-      </TopBarContainer>
-    </HeaderContentContainer>
+    <EDSTopBar>
+      <TopBar.Header>
+        <NextLink href="/" passHref>
+          <Button as="a" variant="ghost" color="secondary">
+            <Typography><FormattedMessage id="navbar.logo" /></Typography>
+          </Button>
+        </NextLink>
+      </TopBar.Header>
+      <EDSCustomContent>
+        <form onSubmit={onSearchSubmit}>
+          <Search aria-label="sitewide" id="search-normal" placeholder={intl.formatMessage({ id: "navbar.placeholderSearch" })} onChange={onSearchChange} value={searchQuery} />
+        </form>
+      </EDSCustomContent>
+      <TopBar.Actions>
+        <nav aria-label={intl.formatMessage({ id: "navbar.ariaUserMenu" })}>
+          <Button variant="ghost_icon" color="secondary">
+            <Icon data={account_circle} title={intl.formatMessage({ id: "navbar.titleAccount" })} />
+          </Button>
+        </nav>
+      </TopBar.Actions>
+
+    </EDSTopBar>
   )
 }
