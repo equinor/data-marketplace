a8ad1c2b71aaec5bdac15660aa79582fe6653aa3
Aron Høyer <aron@hoyer.com>
2022-08-22 10:31:36 +0200

    refactor: use token from next-auth

--- pages/api/popular.ts
+++ pages/api/popular.ts
@@ -1,4 +1,5 @@
 import type { NextApiHandler } from "next"
+import { getToken } from "next-auth/jwt"
 
 import { config } from "../../config"
 import { HttpClient } from "../../lib/HttpClient"
@@ -8,18 +9,20 @@ type PopularAsset = Collibra.Asset & Pick<Collibra.NavigationStatistic, "numberO
 
 const getPopularAssets = async (
   data: PopularAsset[],
-  auth: string,
+  token: string,
   limit: number,
   offset = 0,
 ): Promise<PopularAsset[]> => {
+  console.log("[getPopularAssets] token", token)
+
   const mostViewedStats = await HttpClient.get<Collibra.PagedNavigationStatisticResponse>(`${config.COLLIBRA_BASE_URL}/navigation/most_viewed`, {
-    headers: { authorization: auth },
+    headers: { authorization: `Bearer ${token}` },
     query: { offset: offset * limit, limit, isGuestExcluded: true },
   })
 
   const assetsResponse = await Promise.all(
     mostViewedStats.body?.results.map((stat) => HttpClient.get<Collibra.Asset>(`${config.COLLIBRA_BASE_URL}/assets/${stat.assetId}`, {
-      headers: { authorization: auth },
+      headers: { authorization: `Bearer ${token}` },
     })) ?? [],
   )
 
@@ -38,31 +41,35 @@ const getPopularAssets = async (
 
   if (result.length >= limit) return result.slice(0, limit)
 
-  return getPopularAssets(result, auth, limit, offset + 1)
+  return getPopularAssets(result, token, limit, offset + 1)
 }
 
 const PopularAssetsHandler: NextApiHandler = async (req, res) => {
   if (req.method !== "GET") {
-    res.status(405).end()
-    return
+    return res.status(405).end()
+  }
+
+  const token = await getToken({ req })
+
+  if (!token) {
+    return res.status(401).end()
   }
 
   const limit = Number.isNaN(Number(req.query.limit)) ? undefined : Number(req.query.limit)
 
   if (!limit) {
     console.log("[PopularAssetsHandler] Invalid limit param", req.query.limit)
-    res.status(400).end()
-    return
+    return res.status(400).end()
   }
 
   try {
-    const dataProducts = await getPopularAssets([], req.headers.authorization!, limit)
+    const dataProducts = await getPopularAssets([], token.accessToken as string, limit)
 
-    res.json(dataProducts)
+    return res.json(dataProducts)
   } catch (error) {
     console.log("[PopularAssetsHandler]", error)
     const err = error as HttpError
-    res.status(err.statusCode ?? 500).json(err.body)
+    return res.status(err.statusCode ?? 500).json(err.body)
   }
 }
 
