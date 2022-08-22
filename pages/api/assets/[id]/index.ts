a9fdb6b5b53cb54ce75f3b99a34aafb11b734b1a
Aron Høyer <aron@hoyer.com>
2022-08-22 10:59:35 +0200

    refactor: prefer early return

--- pages/api/assets/[id]/index.ts
+++ pages/api/assets/[id]/index.ts
@@ -6,19 +6,19 @@ import { HttpError } from "../../../../lib/HttpError"
 
 const GetAssetByIDHandler: NextApiHandler = async (req, res) => {
   if (req.method !== "GET") {
-    res.status(405).end()
-  } else {
-    try {
-      const assetRes = await HttpClient.get<Collibra.Asset>(`${config.COLLIBRA_BASE_URL}/assets/${req.query.id}`, {
-        headers: { authorization: req.headers.authorization },
-      })
+    return res.status(405).end()
+  }
 
-      res.json(assetRes.body)
-    } catch (error) {
-      console.log("[GetAssetByIDHandler]", error)
-      const err = error as HttpError
-      res.status(err.statusCode ?? 500).json(err.body)
-    }
+  try {
+    const assetRes = await HttpClient.get<Collibra.Asset>(`${config.COLLIBRA_BASE_URL}/assets/${req.query.id}`, {
+      headers: { authorization: req.headers.authorization },
+    })
+
+    return res.json(assetRes.body)
+  } catch (error) {
+    console.log("[GetAssetByIDHandler]", error)
+    const err = error as HttpError
+    return res.status(err.statusCode ?? 500).json(err.body)
   }
 }
 
--- pages/api/assets/[id]/overview.ts
+++ pages/api/assets/[id]/overview.ts
@@ -7,29 +7,29 @@ import { HttpError } from "../../../../lib/HttpError"
 
 const GetAssetOverview: NextApiHandler = async (req, res) => {
   if (req.method !== "GET") {
-    res.status(405).end()
-  } else {
-    try {
-      const attrsRes = await HttpClient.get<Collibra.PagedAttributeResponse>(`${config.COLLIBRA_BASE_URL}/attributes`, {
-        headers: { authorization: req.headers.authorization },
-        query: { assetId: req.query.id },
-      })
+    return res.status(405).end()
+  }
 
-      const attrs = attrsRes.body?.results.filter((attr) => [
-        "description",
-        "purpose",
-        "timeliness",
-      ].includes(attr.type.name!.toLowerCase())).reduce((obj, attr) => ({
-        ...obj,
-        [attr.type.name!.toLowerCase().replace(/\s/g, "_")]: xss(attr.value),
-      }), {})
+  try {
+    const attrsRes = await HttpClient.get<Collibra.PagedAttributeResponse>(`${config.COLLIBRA_BASE_URL}/attributes`, {
+      headers: { authorization: req.headers.authorization },
+      query: { assetId: req.query.id },
+    })
 
-      res.json(attrs)
-    } catch (error) {
-      console.log("[GetAssetOverviewHandler]", error)
-      const err = error as HttpError
-      res.status(err.statusCode ?? 500).json(err.body)
-    }
+    const attrs = attrsRes.body?.results.filter((attr) => [
+      "description",
+      "purpose",
+      "timeliness",
+    ].includes(attr.type.name!.toLowerCase())).reduce((obj, attr) => ({
+      ...obj,
+      [attr.type.name!.toLowerCase().replace(/\s/g, "_")]: xss(attr.value),
+    }), {})
+
+    return res.json(attrs)
+  } catch (error) {
+    console.log("[GetAssetOverviewHandler]", error)
+    const err = error as HttpError
+    return res.status(err.statusCode ?? 500).json(err.body)
   }
 }
 
