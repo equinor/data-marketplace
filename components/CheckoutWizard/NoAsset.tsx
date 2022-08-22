b59b2faf616e5cc3a1673d1bc23e88cf4333a05b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-08-22 12:03:49 +0200

    ♻️ Reusable component in case of no asset

--- components/CheckoutWizard/NoAsset.tsx
+++ components/CheckoutWizard/NoAsset.tsx
@@ -1,5 +1,20 @@
-import { Container } from "../Container"
+/* eslint-disable camelcase */
+import { Banner, Icon } from "@equinor/eds-core-react"
+import {
+  warning_outlined,
+} from "@equinor/eds-icons"
+import { useIntl } from "react-intl"
 
-export const NoAsset = () => (
-  <Container>No Asset</Container>
-)
+const { Message, Icon: BannerIcon } = Banner
+
+export const NoAsset = () => {
+  const intl = useIntl()
+  return (
+    <Banner elevation="raised">
+      <BannerIcon variant="warning">
+        <Icon data={warning_outlined} />
+      </BannerIcon>
+      <Message>{intl.formatMessage({ id: "checkout.no.asset" })}</Message>
+    </Banner>
+  )
+}
