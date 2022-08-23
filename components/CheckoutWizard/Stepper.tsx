22917c8585dc292b9ce2d8758ce22e0bda9657d2
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-08-23 15:08:16 +0200

    ✨ Add EDS Stepper for navigation
    First version since this is not available as a React component. It can be improved to make it easier to reuse, but we don't want to spend to much time on this now

---
+++ components/CheckoutWizard/Stepper.tsx
@@ -0,0 +1,27 @@
+import { List } from "@equinor/eds-core-react"
+import { useIntl } from "react-intl"
+import styled from "styled-components"
+
+import { StepperItem } from "./StepperItem"
+
+const StyledStepper = styled(List)`
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 1fr 1fr min-content;
+  padding: 0;
+`
+
+type Props = {
+  currentStep: number,
+}
+
+export const Stepper = ({ currentStep }: Props) => {
+  const intl = useIntl()
+  return (
+    <StyledStepper variant="numbered">
+      <StepperItem label={intl.formatMessage({ id: "checkout.nav.step.terms" })} currentStep={currentStep} index={0} />
+      <StepperItem label={intl.formatMessage({ id: "checkout.nav.step.access" })} currentStep={currentStep} index={1} />
+      <StepperItem label={intl.formatMessage({ id: "checkout.nav.step.redirect" })} currentStep={currentStep} index={2} />
+    </StyledStepper>
+  )
+}
