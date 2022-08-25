6fc83784bc5a842a49d7897ccb6accdd3444687a
Aron Høyer <aron@hoyer.com>
2022-08-25 11:22:07 +0200

    styles: fixes some issues with component structure that set unecessary usage requirements

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -11,28 +11,20 @@ type Props = {
 }
 
 const BannerContainer = styled.div<{ variant: string }>`
-    background-color: ${({ variant }) => (variant === "danger" ? tokens.colors.ui.background__danger.rgba : tokens.colors.ui.background__warning.rgba)};
-    display: flex;
-    justify-content: start;
-    gap: 1rem;
-    padding: 1.5rem;
+  background-color: ${({ variant }) => (variant === "danger" ? tokens.colors.ui.background__danger.rgba : tokens.colors.ui.background__warning.rgba)};
+  display: flex;
+  justify-content: start;
+  gap: 1rem;
+  padding: 1.5rem;
 `
 
-const BannerIcon = styled.div`
-    line-height:0;
-`
-const TextWrapper = styled.div`
-display: flex;
-align-items: center;
-`
 export const Banner: FunctionComponent<Props> = ({ variant, children, ...rest }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <BannerContainer variant={variant} {...rest}>
-    <BannerIcon>
-      <Icon color={variant === "danger" ? tokens.colors.interactive.danger__text.rgba : tokens.colors.interactive.warning__text.rgba} data={warning_filled} />
-    </BannerIcon>
-    <TextWrapper>
+    <Icon color={variant === "danger" ? tokens.colors.interactive.danger__text.rgba : tokens.colors.interactive.warning__text.rgba} data={warning_filled} />
+
+    <div>
       {children}
-    </TextWrapper>
+    </div>
   </BannerContainer>
 )
