5fa5745d75ab12ab4bb4a5f486af0b73ee87b533
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-08-29 09:02:22 +0200

    ♿️ Fix missing lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ import {
 import { FunctionComponent } from "react"
 
 const Document: FunctionComponent<DocumentProps> = () => (
-  <Html>
+  <Html lang="en">
     <Head>
       <link rel="stylesheet" href="https://eds-static.equinor.com/font/equinor-font.css" />
       <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
