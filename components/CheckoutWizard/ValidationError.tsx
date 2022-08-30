3da7954353d163e8fa48540d3814eea9dd52cd3d
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-08-30 10:25:05 +0200

    ♻️ Add pro tip from Mickey to get the helper styles

--- components/CheckoutWizard/ValidationError.tsx
+++ components/CheckoutWizard/ValidationError.tsx
@@ -13,22 +13,14 @@ const ValidationContainer = styled.div`
   gap: ${tokens.spacings.comfortable.small};
 `
 
-export const HelperTextStyles = styled(Typography)<{error?: boolean}>`
-/* Stole styles form the helper text since variant="helper" has a runtime error */
-font-size: 0.750rem;
-line-height: 1.33em;
-letter-spacing: 0.013em;
-font-weight: 500;
-
-${({ error }) => error && `
-    color: ${errorColor};
-  `}
+const HelperTextStyles = styled(Typography)`
+  color: ${errorColor};
 `
 
 export const ValidationError: FunctionComponent<PropsWithChildren> = ({ children, ...rest }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <ValidationContainer {...rest}>
     <Icon data={error_filled} color={errorColor} size={16} />
-    <HelperTextStyles error role="alert">{children}</HelperTextStyles>
+    <HelperTextStyles group="input" variant="helper" role="alert">{children}</HelperTextStyles>
   </ValidationContainer>
 )
--- components/CheckoutWizard/index.ts
+++ components/CheckoutWizard/index.ts
@@ -2,6 +2,5 @@ export { CheckoutWizard } from "./CheckoutWizard"
 export { NoAsset } from "./NoAsset"
 export { CancelButton } from "./CancelButton"
 export { ValidationError } from "./ValidationError"
-export { HelperTextStyles } from "./ValidationError"
 
 export type { AssetIdProp } from "./types"
