2d72c19fee646b7d7bfc0bebec5ea64666a3316f
Aron Høyer <aron@hoyer.com>
2022-08-31 14:43:37 +0200

    feat: add user menu popover with sign out option

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,17 +2,20 @@
 import {
   Button,
   Icon,
+  Menu,
   Search,
   TopBar,
   Typography,
 } from "@equinor/eds-core-react"
-import { account_circle } from "@equinor/eds-icons"
+import { account_circle, power } from "@equinor/eds-icons"
+import { signOut } from "next-auth/react"
 import NextLink from "next/link"
 import { useRouter } from "next/router"
 import {
   ChangeEventHandler,
   FormEventHandler,
   useEffect,
+  useRef,
   useState,
 } from "react"
 import { FormattedMessage, useIntl } from "react-intl"
@@ -28,9 +31,12 @@ const EDSCustomContent = styled(TopBar.CustomContent)`
 
 export const NavBar = () => {
   const [searchQuery, setSearchQuery] = useState<string>("")
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState<boolean>(false)
 
   const router = useRouter()
   const intl = useIntl()
+
+  const userMenuAnchor = useRef<HTMLButtonElement>(null)
 
   useEffect(() => {
     setSearchQuery(router.query.q as string ?? "")
@@ -51,6 +57,12 @@ export const NavBar = () => {
     setSearchQuery(e.target.value)
   }
 
+  const onUserMenuAnchorClick = () => {
+    setIsUserMenuOpen(!isUserMenuOpen)
+  }
+
+  const onSignOutClick = () => signOut()
+
   return (
     <EDSTopBar>
       <TopBar.Header>
@@ -67,12 +79,32 @@ export const NavBar = () => {
       </EDSCustomContent>
       <TopBar.Actions>
         <nav aria-label={intl.formatMessage({ id: "navbar.ariaUserMenu" })}>
-          <Button variant="ghost_icon" color="secondary">
+          <Button
+            ref={userMenuAnchor}
+            aria-controls="user-menu"
+            aria-haspopup="true"
+            id="user-menu-anchor"
+            variant="ghost_icon"
+            color="secondary"
+            onClick={onUserMenuAnchorClick}
+          >
             <Icon data={account_circle} title={intl.formatMessage({ id: "navbar.titleAccount" })} />
           </Button>
+
+          <Menu
+            anchorEl={userMenuAnchor.current}
+            aria-labelledby="user-menu-anchor"
+            id="user-menu"
+            placement="bottom-end"
+            open={isUserMenuOpen}
+          >
+            <Menu.Item onClick={onSignOutClick}>
+              <Icon data={power} />
+              Sign out
+            </Menu.Item>
+          </Menu>
         </nav>
       </TopBar.Actions>
-
     </EDSTopBar>
   )
 }
