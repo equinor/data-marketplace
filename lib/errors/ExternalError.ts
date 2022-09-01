7db736c34cd872ca13aed21dd641b0a25da298f5
Aron Høyer <aron@hoyer.com>
2022-09-01 10:53:52 +0200

    feat: add external error utility class

---
+++ lib/errors/ExternalError.ts
@@ -0,0 +1,12 @@
+import { __Error__ } from "./__Error__"
+import { ErrSource } from "./types"
+
+/**
+ * ExternalError should be thrown when for example a call to an
+ * external service returns unexpected data or fails outright.
+ */
+export class ExternalError extends __Error__ {
+  constructor(message: string, code: string) {
+    super(message, ErrSource.External, code)
+  }
+}
