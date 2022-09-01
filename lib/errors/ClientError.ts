9e28e022230abac7cab4291a0e746d37a04e84f0
Aron Høyer <aron@hoyer.com>
2022-09-01 13:10:01 +0200

    feat: add optional details arg

--- lib/errors/ClientError.ts
+++ lib/errors/ClientError.ts
@@ -2,7 +2,7 @@ import { __Error__ } from "./__Error__"
 import { ErrSource } from "./types"
 
 export class ClientError extends __Error__ {
-  constructor(message: string, code: string) {
-    super(message, ErrSource.Client, code)
+  constructor(message: string, code: string, details?: any) {
+    super(message, ErrSource.Client, code, details)
   }
 }
