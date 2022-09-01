3c39a9d1703f5fdad75736a5bcb29f9e3df6a469
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-01 15:04:52 +0200

    💬 Use text snippets

--- components/AssetTabContent/ResponsibilitiesContent.tsx
+++ components/AssetTabContent/ResponsibilitiesContent.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "@equinor/eds-core-react"
 import { tokens } from "@equinor/eds-tokens"
+import { useIntl } from "react-intl"
 import styled from "styled-components"
 
 import { ResponsibilitiesHolderList, ResponsibilityHolder } from "./ResponsibilitiesHolderList"
@@ -23,27 +24,35 @@ type Props = {
   content?: ResponsibilitiesContentSections
 }
 
-export const ResponsibilitiesContent = ({ content }: Props) => (
+export const ResponsibilitiesContent = ({ content }: Props) => {
+  const intl = useIntl()
 
-  <ResponsibilitiesWrapper>
-    <Intro>
-      <Typography style={{ marginBottom: tokens.spacings.comfortable.small }} variant="h3" as="h2">Get in touch</Typography>
-      <Typography variant="body_long">Have a question about this data product? Get in touch with the data steward if you have any general questions. Technical questions or issues, contact the technical steward..</Typography>
-    </Intro>
-    <Responsibilities>
-      {content?.DATA_STEWARD && (
-        <ResponsibilitiesHolderList headline="Data stewards" holders={content.DATA_STEWARD} />
-      )}
-      {content?.TECHNICAL_STEWARD && (
-        <ResponsibilitiesHolderList headline="Technical stewards" holders={content.TECHNICAL_STEWARD} />
-      )}
-      {content?.OWNER && (
-        <ResponsibilitiesHolderList headline="Owners" holders={content.OWNER} />
-      )}
+  return (
 
-      {content?.DATA_OFFICE_ADMIN && (
-        <ResponsibilitiesHolderList headline="Data office admins" holders={content.DATA_OFFICE_ADMIN} />
-      )}
-    </Responsibilities>
-  </ResponsibilitiesWrapper>
-)
+    <ResponsibilitiesWrapper>
+      <Intro>
+        <Typography style={{ marginBottom: tokens.spacings.comfortable.small }} variant="h3" as="h2">
+          {intl.formatMessage({ id: "responsibility.intro.header" })}
+        </Typography>
+        <Typography variant="body_long">
+          {intl.formatMessage({ id: "responsibility.intro.ingress" })}
+        </Typography>
+      </Intro>
+      <Responsibilities>
+        {content?.DATA_STEWARD && (
+          <ResponsibilitiesHolderList headline="Data stewards" holders={content.DATA_STEWARD} />
+        )}
+        {content?.TECHNICAL_STEWARD && (
+          <ResponsibilitiesHolderList headline="Technical stewards" holders={content.TECHNICAL_STEWARD} />
+        )}
+        {content?.OWNER && (
+          <ResponsibilitiesHolderList headline="Owners" holders={content.OWNER} />
+        )}
+
+        {content?.DATA_OFFICE_ADMIN && (
+          <ResponsibilitiesHolderList headline="Data office admins" holders={content.DATA_OFFICE_ADMIN} />
+        )}
+      </Responsibilities>
+    </ResponsibilitiesWrapper>
+  )
+}
