277054b4e653c63ef90787cdd7641b0de44c6eb9
Aron Høyer <aron@hoyer.com>
2022-09-01 13:43:44 +0200

    refactor: remove InternalError class

--- lib/errors/index.ts
+++ lib/errors/index.ts
@@ -1,4 +1,3 @@
 export { ERR_CODES } from "./codes"
 export { ClientError } from "./ClientError"
 export { ExternalError } from "./ExternalError"
-export { InternalError } from "./InternalError"
--- lib/errors/types.ts
+++ lib/errors/types.ts
@@ -1,5 +1,4 @@
 export enum ErrSource {
   Client,
   External,
-  Internal,
 }
