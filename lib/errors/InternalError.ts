d670ebc4708c3048d28eb0c6d6c97c1d1afd90c4
Aron Høyer <aron@hoyer.com>
2022-09-01 13:43:44 +0200

    refactor: remove InternalError class

--- lib/errors/InternalError.ts
+++
@@ -1,8 +0,0 @@
-import { __Error__ } from "./__Error__"
-import { ErrSource } from "./types"
-
-export class InternalError extends __Error__ {
-  constructor(message: string, code: string, details?: any) {
-    super(message, ErrSource.Internal, code, details)
-  }
-}
