51074720ded62850297c3b522416b79cfa6db63b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-06 15:36:04 +0200

    🔍 Add fallback title

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -1,4 +1,6 @@
+import Head from "next/head"
 import { FunctionComponent, ReactNode } from "react"
+import { useIntl } from "react-intl"
 
 import { NavBar } from "../NavBar"
 
@@ -6,9 +8,18 @@ type Props = {
   children: ReactNode
 };
 
-export const Page: FunctionComponent<Props> = ({ children }) => (
-  <>
-    <NavBar />
-    {children}
-  </>
-)
+export const Page: FunctionComponent<Props> = ({ children }) => {
+  const intl = useIntl()
+
+  return (
+    <>
+      <Head>
+        <title>
+          {intl.formatMessage({ id: "common.documentTitle" })}
+        </title>
+      </Head>
+      <NavBar />
+      {children}
+    </>
+  )
+}
