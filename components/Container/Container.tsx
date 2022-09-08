fe24f1af78035ad69f8f82efbc3702235f29850b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-08 10:33:31 +0200

    ♻️ Use CSS variable for Container

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -2,20 +2,7 @@ import styled from "styled-components"
 
 export const Container = styled.div`
   width: 100%;
-  padding: 0 1rem;
   margin: 0 auto;
-
-  @media screen and (min-width: 768px) {
-    padding: 0 1.5rem;
-  }
-
-  @media screen and (min-width: 992px) {
-    padding: 0 2rem;
-    max-width: 70rem;
-  }
-
-  @media screen and (min-width: 1200px) {
-    padding: 0 4rem;
-    max-width: 80rem;
-  }
+  padding-inline: var(--layout-padding-inline);
+  max-width: var(--layout-max-width);
 `
