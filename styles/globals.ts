0cd5bbddb3f27b7ca78d1a97b8117ae845e73dae
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-09 09:59:45 +0200

    💄 Add block padding to section component

--- styles/globals.ts
+++ styles/globals.ts
@@ -7,7 +7,6 @@ body {
   padding: 0;
   margin: 0;
   font-family: Equinor, sans-serif;
-  
 }
 
 a {
@@ -21,20 +20,24 @@ a {
 
 :root {
   --layout-padding-inline: ${tokens.spacings.comfortable.medium};
+  --layout-padding-block: ${tokens.spacings.comfortable.x_large};
   --layout-max-width: none;
   --highlight-colour: ${tokens.colors.infographic.primary__moss_green_13.hex};
 
   @media screen and (min-width: 768px) {
     --layout-padding-inline: ${tokens.spacings.comfortable.large};
+    --layout-padding-block: ${tokens.spacings.comfortable.xx_large};
   }
 
   @media screen and (min-width: 992px) {
     --layout-padding-inline: ${tokens.spacings.comfortable.x_large};
+    --layout-padding-block: ${tokens.spacings.comfortable.xxx_large};
     --layout-max-width: 70rem;
   }
 
   @media screen and (min-width: 1200px) {
     --layout-padding-inline: 4rem;
+    --layout-padding-block: 5rem;
     --layout-max-width: 80rem;
   }
 }
