bece68562a1eaf05dcfa64df0dc589be24756132
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-09 11:35:42 +0200

    ♻️ Move the page wrapper to the Page

--- components/AuthContainer/AuthContainer.tsx
+++ components/AuthContainer/AuthContainer.tsx
@@ -2,18 +2,8 @@ import { Typography } from "@equinor/eds-core-react"
 import { signIn, useSession } from "next-auth/react"
 import type { FunctionComponent, PropsWithChildren } from "react"
 import { useIntl } from "react-intl"
-import styled from "styled-components"
 
 import { Container } from "../Container"
-
-const PageWrapper = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  &:has(.background-highlight:last-child) {
-    background-color: var(--highlight-colour);
-  } 
-`
 
 export const AuthContainer: FunctionComponent<PropsWithChildren> = ({ children }) => {
   const { data, status } = useSession()
@@ -38,6 +28,7 @@ export const AuthContainer: FunctionComponent<PropsWithChildren> = ({ children }
   }
 
   return (
-    <PageWrapper>{children}</PageWrapper>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>{children}</>
   )
 }
--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -1,9 +1,19 @@
 import Head from "next/head"
 import { FunctionComponent, ReactNode } from "react"
 import { useIntl } from "react-intl"
+import styled from "styled-components"
 
 import { Footer } from "../Footer"
 import { NavBar } from "../NavBar"
+
+const PageWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  &:has(.background-highlight:last-child) {
+    background-color: var(--highlight-colour);
+  } 
+`
 
 type Props = {
   children: ReactNode
@@ -13,7 +23,7 @@ export const Page: FunctionComponent<Props> = ({ children }) => {
   const intl = useIntl()
 
   return (
-    <>
+    <PageWrapper>
       <Head>
         <title>
           {intl.formatMessage({ id: "common.documentTitle" })}
@@ -23,6 +33,6 @@ export const Page: FunctionComponent<Props> = ({ children }) => {
 
       {children}
       <Footer />
-    </>
+    </PageWrapper>
   )
 }
