65a5cffc23bae806f2c2d13bc2cec770988eb381
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-09 10:24:28 +0200

    ✨ Add top padding algorithm
     If a background is a direct sibling of a background  with the same colour there should be no top padding

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -2,25 +2,30 @@ import { tokens } from "@equinor/eds-tokens"
 import type { FunctionComponent, ReactNode } from "react"
 import styled from "styled-components"
 
-const StyledSection = styled.section`
-    width: 100%;
-    max-width: var(--layout-max-width);
-    margin-inline: auto;
-    padding-bottom: 2rem;
-    padding: var(--layout-padding-block) var(--layout-padding-inline);
-`
-
 type BackgroundProps = {
   isHighlighted: boolean
 }
 
 const BackgroundContainer = styled.div.attrs<BackgroundProps>(({ isHighlighted }) => (isHighlighted
- && {
-   className: "background-highlight",
- }
+  ? {
+    className: "background-highlight",
+  } : {
+    className: "background-default",
+  }
 
 ))<BackgroundProps>`
   background-color: ${({ isHighlighted }) => (isHighlighted ? "var(--highlight-colour)" : tokens.colors.ui.background__default.hex)} ;
+`
+
+const StyledSection = styled.section`
+    width: 100%;
+    max-width: var(--layout-max-width);
+    margin-inline: auto;
+    padding: var(--layout-padding-block) var(--layout-padding-inline);
+    ${BackgroundContainer}.background-default + ${BackgroundContainer}.background-default &,
+    ${BackgroundContainer}.background-highlight + ${BackgroundContainer}.background-highlight & {
+      padding-top: 0;
+    }
 `
 
 type Props = {
