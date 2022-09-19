4a38b7caaf30af2c71af9a9877514547ddc4b48b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-19 17:39:07 +0200

    🔧 Add appinsight config to CI

--- appInsights.ts
+++ appInsights.ts
@@ -1,6 +1,7 @@
 import { ReactPlugin } from "@microsoft/applicationinsights-react-js"
 import { ApplicationInsights } from "@microsoft/applicationinsights-web"
 
+import { config } from "config"
 /* let history: History | null = null
 
 if (typeof window !== "undefined") {
@@ -10,7 +11,7 @@ if (typeof window !== "undefined") {
 const reactPlugin = new ReactPlugin()
 const appInsights = new ApplicationInsights({
   config: {
-    connectionString: process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION_STRING || "",
+    connectionString: config.INSIGHTS_CONNECTION_STRING,
     extensions: [reactPlugin],
     /* extensionConfig: {
       [reactPlugin.identifier]: { history },
