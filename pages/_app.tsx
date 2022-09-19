b96ea502c58f25c6b9f4af59c77e394b0c833e2e
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-19 12:58:18 +0200

    ⚗ Test with App Insights Error Boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppInsightsContext } from "@microsoft/applicationinsights-react-js"
+import { AppInsightsContext, AppInsightsErrorBoundary } from "@microsoft/applicationinsights-react-js"
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import Head from "next/head"
@@ -6,24 +6,30 @@ import { IntlProvider } from "react-intl"
 
 import { reactPlugin } from "appInsights"
 import { AuthContainer } from "components/AuthContainer"
+import { ErrorBoundary } from "components/ErrorBoundary"
 import englishTexts from "locales/english.json"
 import { GlobalStyle } from "styles/globals"
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
-  <SessionProvider session={session}>
-    <IntlProvider locale="en" defaultLocale="en" messages={englishTexts}>
-      <AppInsightsContext.Provider value={reactPlugin}>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <GlobalStyle />
-        <AuthContainer>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...pageProps} />
-        </AuthContainer>
-      </AppInsightsContext.Provider>
-    </IntlProvider>
-  </SessionProvider>
+  <AppInsightsErrorBoundary
+    onError={ErrorBoundary}
+    appInsights={reactPlugin}
+  >
+    <SessionProvider session={session}>
+      <IntlProvider locale="en" defaultLocale="en" messages={englishTexts}>
+        <AppInsightsContext.Provider value={reactPlugin}>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <GlobalStyle />
+          <AuthContainer>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <Component {...pageProps} />
+          </AuthContainer>
+        </AppInsightsContext.Provider>
+      </IntlProvider>
+    </SessionProvider>
+  </AppInsightsErrorBoundary>
 )
 
 export default App
