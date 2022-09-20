e67da3163f3744869f64c51e62a5b49fc7b39efd
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-20 11:10:20 +0200

    💄 Improve the error boundary page
    Doesn't want to spend to much time here

--- components/ErrorBoundary/ErrorBoundary.tsx
+++ components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,3 +1,20 @@
+import { Typography } from "@equinor/eds-core-react"
+
+import { Section } from "../Section"
+
+import { GlobalStyle } from "styles/globals"
+
 export const ErrorBoundary = () => (
-  <h1>Something went wrong, omg</h1>
+  <>
+    <GlobalStyle />
+    <Section>
+      <Typography variant="h1" style={{ marginBottom: "0.83em" }}>Something went wrong</Typography>
+      <Typography style={{ maxWidth: "30rem" }} variant="body_long">
+        The application detected an error that prevented it from loading.
+        This error has been automatically reported
+        to the development team.
+      </Typography>
+    </Section>
+  </>
+
 )
