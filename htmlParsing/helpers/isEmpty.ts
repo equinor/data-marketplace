1464b9e24ab1d8a9f57ffbbcec3b88f7e8d76986
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-27 14:16:47 +0200

    🚸 Try to avoid empty p tags

---
+++ htmlParsing/helpers/isEmpty.ts
@@ -0,0 +1,4 @@
+// @TODO Not able to figure out exactly the types
+export const isEmpty = (children: any[]) => children.every(
+  (child) => child.length === 0 || child === String.fromCharCode(160),
+)
