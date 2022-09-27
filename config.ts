b5e312a7a2506bac7f8a35c61ab61f877667e4c6
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-27 14:32:14 +0200

    🐛 Fix copy'n paste error

--- config.ts
+++ config.ts
@@ -10,5 +10,5 @@ export const config = Object.freeze({
 
   ACCESSIT_BASE_URL: "https://accessit.equinor.com",
 
-  USE_PORTABLE_TEXT = process.env.NEXT_PUBLIC_USE_PORTABLE_TEXT === "true",
+  USE_PORTABLE_TEXT: process.env.NEXT_PUBLIC_USE_PORTABLE_TEXT === "true",
 })
