5305b5bd09be7ea3ea57ecb77a2f0e0bad35856e
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-09-29 15:40:52 +0200

    ✨ Parse thead and render as EDS Head

--- htmlParsing/portableText/Table.tsx
+++ htmlParsing/portableText/Table.tsx
@@ -1,9 +1,25 @@
 import { Table as EdsTable } from "@equinor/eds-core-react"
 
-const { Body, Row, Cell } = EdsTable
+const {
+  Body, Row, Cell, Head,
+} = EdsTable
 
 export const Table = ({ value }: any) => (
   <EdsTable>
+    {value.headerRow
+    && (
+      <Head>
+        {value.headerRow.map((row: any) => (
+          <Row key={row.key}>
+            {row.cells.map((cell: any) => (
+              <Cell key={cell.key}>
+                {cell.text.trim()}
+              </Cell>
+            ))}
+          </Row>
+        ))}
+      </Head>
+    )}
     <Body>
       {value.rows.map((row: any) => (
         <Row key={row.key}>
