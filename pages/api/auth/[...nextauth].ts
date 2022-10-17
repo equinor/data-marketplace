188599d55048d292b6faa81466476f7cb9b341c0
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-10-17 11:36:24 +0200

    🔧 Test without the trailing slash

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,20 +9,20 @@ import { HttpClient } from "lib/HttpClient"
 const SCOPE = `openid offline_access ${config.AUTH_SCOPE}`
 
 type Token = JWT & {
-  tokenType: string,
-  accessToken: string,
-  expiresAt: number,
-  refreshToken: string,
+  tokenType: string
+  accessToken: string
+  expiresAt: number
+  refreshToken: string
   error?: string
 }
 
 type RefreshTokenResponse = {
   token_type: string
   scope: string
-  expires_in: number,
-  ext_expires_in: number,
-  access_token: string,
-  refresh_token: string,
+  expires_in: number
+  ext_expires_in: number
+  access_token: string
+  refresh_token: string
   id_token: string
 }
 
@@ -30,17 +30,20 @@ const tryAccessTokenRefresh = async (token: Token): Promise<Token> => {
   console.log("[NextAuth] Attempting to refresh access token")
 
   try {
-    const res = await HttpClient.post<RefreshTokenResponse>(`https://login.microsoftonline.com/${config.AUTH_TENANT_ID}/oauth2/v2.0/token`, {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: qs.stringify({
-        client_id: config.AUTH_CLIENT_ID,
-        scope: SCOPE,
-        redirect_uri: `${config.BASE_URL}/api/auth/callback/azure-ad`,
-        grant_type: "refresh_token",
-        client_secret: config.AUTH_CLIENT_SECRET,
-        refresh_token: token.refreshToken,
-      }),
-    })
+    const res = await HttpClient.post<RefreshTokenResponse>(
+      `https://login.microsoftonline.com/${config.AUTH_TENANT_ID}/oauth2/v2.0/token`,
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: qs.stringify({
+          client_id: config.AUTH_CLIENT_ID,
+          scope: SCOPE,
+          redirect_uri: `${config.BASE_URL}/api/auth/callback/azure-ad`,
+          grant_type: "refresh_token",
+          client_secret: config.AUTH_CLIENT_SECRET,
+          refresh_token: token.refreshToken,
+        }),
+      }
+    )
 
     if (!res.body) {
       throw new Error(`No body in response from ${res.headers.origin}`)
