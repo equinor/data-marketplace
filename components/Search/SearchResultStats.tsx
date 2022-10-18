9461076ff04ad72c5da14eefc356e5e9ccf7e89f
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-10-18 09:26:43 +0200

    🚸 Fix flash of no search text if search is slow

--- components/Search/SearchResultStats.tsx
+++ components/Search/SearchResultStats.tsx
@@ -2,21 +2,23 @@ import { FunctionComponent } from "react"
 import { FormattedMessage } from "react-intl"
 
 type Props = {
-  numberOfHits: number,
-  numberOfFilters: number,
+  numberOfHits: number | null
+  numberOfFilters: number
   query: string | string[]
 }
 
-export const SearchResultStats: FunctionComponent<Props> = (
-  { numberOfHits, numberOfFilters, query },
-) => {
+// Sorry future self for way too many returns here, but
+// this logic will be completely different with Algolia
+export const SearchResultStats: FunctionComponent<Props> = ({ numberOfHits, numberOfFilters, query }) => {
+  if (numberOfHits === null) return null
+  // No results and that's probably because of the use of community filter/s
   if (numberOfHits === 0 && numberOfFilters > 0) {
     return (
       <FormattedMessage
         id="search.no.results.with.filters"
         values={{
           numberOfFilters,
-          searchTerm: (<b>{query}</b>),
+          searchTerm: <b>{query}</b>,
         }}
       />
     )
@@ -26,7 +28,7 @@ export const SearchResultStats: FunctionComponent<Props> = (
       id="search.results"
       values={{
         count: numberOfHits,
-        searchTerm: (<b>{query}</b>),
+        searchTerm: <b>{query}</b>,
       }}
     />
   )
