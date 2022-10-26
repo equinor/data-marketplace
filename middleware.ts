0371b3691380978868e08103d1622fca16fda3be
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-10-26 16:08:17 +0200

    🔧 Add search to authenticated pages

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-restricted-exports
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/", "/assets/:path*", "/checkout/:path*"] }
+export const config = { matcher: ["/", "/assets/:path*", "/checkout/:path*", "/search"] }
