8b6b6ea30e71b79149f7ec1c73a2470d78fd5463
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-10-27 13:59:40 +0200

    ✨ Add flow for a first time visitor

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 export const config = Object.freeze({
   BASE_URL: process.env.BASE_URL ?? "http://localhost:3000",
   COLLIBRA_BASE_URL: process.env.NEXT_PUBLIC_COLLIBRA_BASE_URL ?? "",
+  COLLIBRA_FIRST_TIME_VISITOR: process.env.NEXT_PUBLIC_COLLIBRA_FIRST_TIME_VISITOR ?? "",
   INSIGHTS_CONNECTION_STRING: process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION_STRING ?? "",
 
   /* Auth config */
