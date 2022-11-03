f8ea2e234ce59621fb6289ffe9fb8f7a8dc22491
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-11-03 11:18:33 +0100

    ✨ Fetch the authorization URL server side for redirect

--- pages/checkout/redirect.tsx
+++ pages/checkout/redirect.tsx
@@ -2,7 +2,8 @@
 import { Button, Icon, Typography } from "@equinor/eds-core-react"
 import { external_link } from "@equinor/eds-icons"
 import { tokens } from "@equinor/eds-tokens"
-import type { NextPage } from "next/types"
+import { getToken } from "next-auth/jwt"
+import type { NextPage, GetServerSideProps } from "next/types"
 import { useIntl } from "react-intl"
 import styled from "styled-components"
 
@@ -10,14 +11,29 @@ import { CheckoutWizard, NoAsset, formatCheckoutTitle } from "components/Checkou
 import { Page } from "components/Page"
 import { config } from "config"
 import { useCheckoutData } from "hooks"
+import { ClientError, ERR_CODES, ExternalError } from "lib/errors"
+import { __Error__ } from "lib/errors/__Error__"
+import { Asset } from "model/Asset"
+import { makeCollibraService } from "services"
+import { getAssetByID, getRightsToUse } from "services/collibra"
 
 const StyledButton = styled(Button)`
   margin-top: ${tokens.spacings.comfortable.x_large};
 `
 
-const CheckoutRedirectView: NextPage = () => {
+type Props = {
+  asset?: Collibra.Asset | null
+  error?: string
+  authorizationUrl?: {
+    value: string
+  }
+}
+
+const CheckoutRedirectView: NextPage<Props> = ({ authorizationUrl }) => {
   const intl = useIntl()
   const { checkoutData } = useCheckoutData()
+
+  const checkoutUrl = authorizationUrl?.value || config.ACCESSIT_BASE_URL
 
   return (
     <Page
@@ -33,7 +49,7 @@ const CheckoutRedirectView: NextPage = () => {
           ) : (
             <>
               <Typography variant="ingress">{intl.formatMessage({ id: "checkout.redirect.headline" })}</Typography>
-              <StyledButton link href={config.ACCESSIT_BASE_URL} target="_blank" rel="noopener noreferrer nofollow">
+              <StyledButton link href={checkoutUrl} target="_blank" rel="noopener noreferrer nofollow">
                 {intl.formatMessage({ id: "checkout.redirect.button.text" })}
                 <Icon data={external_link} />
               </StyledButton>
@@ -43,6 +59,65 @@ const CheckoutRedirectView: NextPage = () => {
       </main>
     </Page>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
+  const { id } = query
+
+  const defaultPageProps: Props = { asset: null }
+
+  if (typeof id !== "string") {
+    return { props: defaultPageProps }
+  }
+
+  const token = await getToken({ req })
+
+  if (typeof token?.accessToken !== "string") {
+    return { props: defaultPageProps }
+  }
+
+  const authorization = `Bearer ${token!.accessToken}`
+  const makeCollibraServiceRequest = makeCollibraService({ authorization })
+
+  try {
+    const asset = await makeCollibraServiceRequest(getAssetByID)(id)
+
+    if (!asset.approved || !Asset.isDataProduct(asset)) {
+      throw new ClientError(`Data product ${id} not approved`, ERR_CODES.ASSET_NOT_APPROVED)
+    }
+
+    const rtuAttrs = await makeCollibraServiceRequest(getRightsToUse)(id)
+    const authorizationUrl = rtuAttrs.find((attr) => attr.type.toLowerCase() === "authorization url")
+
+    if (!authorizationUrl) {
+      throw new ExternalError(`No authorization URL found for ${asset.name} (ID: ${asset.id})`, ERR_CODES.MISSING_DATA)
+    }
+
+    return {
+      props: {
+        asset: JSON.parse(JSON.stringify(asset)),
+        authorizationUrl: {
+          value: authorizationUrl.value,
+        },
+      },
+    }
+  } catch (error) {
+    console.error("[CheckoutRedirectView]", error)
+
+    // if (error instanceof ExternalError || error instanceof ClientError) {
+    if (error instanceof __Error__) {
+      return {
+        props: {
+          ...defaultPageProps,
+          error: error.code,
+        },
+      }
+    }
+
+    return {
+      props: defaultPageProps,
+    }
+  }
 }
 
 export default CheckoutRedirectView
