9ae7bf36b3969bc2d2e3e17b8b1581c7479ccda7
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-11-03 09:30:11 +0100

    ♻️ Implement new redirect step

--- pages/checkout/redirect.tsx
+++ pages/checkout/redirect.tsx
@@ -1,8 +1,9 @@
-import { Progress, Typography } from "@equinor/eds-core-react"
+/* eslint-disable camelcase */
+import { Button, Icon, Typography } from "@equinor/eds-core-react"
+import { external_link } from "@equinor/eds-icons"
 import { tokens } from "@equinor/eds-tokens"
 import type { NextPage } from "next/types"
-import { ReactNode, useCallback, useEffect, useState } from "react"
-import { FormattedMessage, useIntl } from "react-intl"
+import { useIntl } from "react-intl"
 import styled from "styled-components"
 
 import { CheckoutWizard, NoAsset, formatCheckoutTitle } from "components/CheckoutWizard"
@@ -10,46 +11,13 @@ import { Page } from "components/Page"
 import { config } from "config"
 import { useCheckoutData } from "hooks"
 
-const HelpText = styled(Typography)`
-  margin-bottom: 1.5rem;
-`
-
-const Redirect = styled.div`
-  margin-block: ${tokens.spacings.comfortable.xx_large};
+const StyledButton = styled(Button)`
+  margin-top: ${tokens.spacings.comfortable.x_large};
 `
 
 const CheckoutRedirectView: NextPage = () => {
-  const [progress, setProgress] = useState<number>(0)
   const intl = useIntl()
   const { checkoutData } = useCheckoutData()
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((p) => {
-        if (p === 100) return 100
-        return p + 2
-      })
-    }, 100)
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (progress === 100) {
-      window.open(config.ACCESSIT_BASE_URL, "_blank")
-    }
-  }, [progress])
-
-  const FormattedLink = useCallback(
-    (chunks: ReactNode[]) => (
-      <Typography link href={config.ACCESSIT_BASE_URL} target="_blank" rel="noopener noreferrer nofollow">
-        {chunks}
-      </Typography>
-    ),
-    []
-  )
 
   return (
     <Page
@@ -65,25 +33,12 @@ const CheckoutRedirectView: NextPage = () => {
           ) : (
             <>
               <Typography variant="ingress">{intl.formatMessage({ id: "checkout.redirect.headline" })}</Typography>
-              <Redirect>
-                <Typography variant="caption">{intl.formatMessage({ id: "checkout.redirect.label" })}</Typography>
-                <Progress.Linear
-                  variant="determinate"
-                  value={progress}
-                  aria-label={intl.formatMessage({ id: "checkout.redirect.progess.arialabel" })}
-                />
-              </Redirect>
+              <StyledButton link href={config.ACCESSIT_BASE_URL} target="_blank" rel="noopener noreferrer nofollow">
+                {intl.formatMessage({ id: "checkout.redirect.button.text" })}
+                <Icon data={external_link} />
+              </StyledButton>
             </>
           )}
-          <HelpText>
-            <FormattedMessage
-              id="checkout.redirect.body"
-              values={{
-                a: FormattedLink,
-                link: config.ACCESSIT_BASE_URL,
-              }}
-            />
-          </HelpText>
         </CheckoutWizard>
       </main>
     </Page>
