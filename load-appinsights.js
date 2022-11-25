a07863692aa5c45fa2777c7d74821c2bb5a7cbcd
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-11-25 15:02:15 +0100

    ⚗ Dotenv experiment

--- load-appinsights.js
+++ load-appinsights.js
@@ -1,7 +1,12 @@
 const appInsights = require("applicationinsights")
+require("dotenv").config()
+
+console.log(process.env)
 
 appInsights
-  .setup("InstrumentationKey=e4d53b02-e08f-45e0-8632-7a066b44bc4f;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/")
+  .setup(
+    "InstrumentationKey=e4d53b02-e08f-45e0-8632-7a066b44bc4f;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/"
+  )
   .setAutoCollectConsole(true, true)
   .setAutoCollectDependencies(true)
   .setAutoCollectExceptions(true)
