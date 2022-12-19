0bead88152839abaec13c89305cd7d1fa6641f2b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-12-19 15:11:44 +0100

    🐛 Fix correct fallback for missing collibra base url

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ type ConfigType = { [key: string]: string | boolean }
 export const config: ConfigType = {
   BASE_URL: process.env.BASE_URL ?? "http://localhost:3000",
   COLLIBRA_BASE_URL: process.env.COLLIBRA_BASE_URL ?? "",
-  COLLIBRA_API_URL: `${process.env.COLLIBRA_BASE_URL}/rest/2.0` || "",
+  COLLIBRA_API_URL: process.env.COLLIBRA_BASE_URL ? `${process.env.COLLIBRA_BASE_URL}/rest/2.0` : "",
   INSIGHTS_CONNECTION_STRING:
     "InstrumentationKey=e4d53b02-e08f-45e0-8632-7a066b44bc4f;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/" ??
     "",
