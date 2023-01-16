f3c27fdddea240ebf9a26b8c91ed95c8f9353a55
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-01-16 09:04:44 +0100

    🔥 Remove collibra service code
    I thought we used this for the search, but it doesn't look like that

--- services/collibra/getAssetByID.ts
+++
@@ -1,7 +0,0 @@
-import { Asset } from "model/Asset"
-import { ServiceRequester } from "services"
-
-export const getAssetByID: ServiceRequester<Asset> = (client) => async (id: string) => {
-  const { body: asset } = await client.get<Collibra.Asset>(`/assets/${id}`)
-  return Asset.fromCollibraAsset(asset)
-}
