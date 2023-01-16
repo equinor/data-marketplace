23cc29a5b30358198256cecd619be40a9df99dde
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-01-16 09:21:56 +0100

    🔥 Delete unused adapter code

--- services/adapter/getAssetMaintainers.ts
+++
@@ -1,8 +0,0 @@
-import type { Maintainer } from "@equinor/data-marketplace-models"
-
-import type { __HttpClient__ } from "lib/HttpClient"
-
-export const getAssetMaintainers = (client: __HttpClient__) => async (assetID: string) => {
-  const { body } = await client.get<Maintainer[]>(`/assets/${assetID}/maintainers`)
-  return body
-}
