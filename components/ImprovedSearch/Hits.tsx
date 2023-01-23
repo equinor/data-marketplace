6eb78b99438183084f4d4704493e4b64d18d5efa
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-01-23 08:18:49 +0100

    🔥 Remove unused imports

--- components/ImprovedSearch/Hits.tsx
+++ components/ImprovedSearch/Hits.tsx
@@ -1,12 +1,36 @@
-import type { Hit } from "instantsearch.js"
-import type { SendEventForHits } from "instantsearch.js/es/lib/utils"
-import { Hits as AlgoliaHits } from "react-instantsearch-hooks-web"
+import { List } from "@equinor/eds-core-react"
+import { useHits } from "react-instantsearch-hooks-web"
+import styled from "styled-components"
 
+import { Hit } from "components/ImprovedSearch"
+
+const { Item } = List
+
+const HitItem = styled(Item)`
+  margin-block: 1rem;
+`
+
+const StyledList = styled(List)`
+  list-style-type: none;
+`
+/* 
 type Props = {
   hitComponent: React.JSXElementConstructor<{
     hit: Hit
     sendEvent: SendEventForHits
   }>
-}
+} */
 
-export const Hits = ({ hitComponent }: Props) => <AlgoliaHits hitComponent={hitComponent} />
+export const Hits = () => {
+  const { hits } = useHits()
+  return (
+    <StyledList variant="numbered">
+      {hits.map((hit) => (
+        <HitItem key={hit.objectID}>
+          {/* @ts-ignore @TODO se på typene her */}
+          <Hit hit={hit} />
+        </HitItem>
+      ))}
+    </StyledList>
+  )
+}
