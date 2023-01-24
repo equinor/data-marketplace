dc82e0f210f655b798ad00b19309e4be2ad81fd9
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-01-24 11:04:51 +0100

    ⚗ Test scroll restoration config

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const withBundle = withBundleAnalyzer({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   compiler: {
     styledComponents: true,
   },
