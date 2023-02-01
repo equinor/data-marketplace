f7f6447df70b05069364c6db22b246e2e2b9c916
Aron Høyer <aron@hoyer.com>
2023-02-01 10:35:41 +0100

    fix: Retry session fetch if requests fail with 401

--- pages/api/communities.ts
+++ pages/api/communities.ts
@@ -1,50 +1,34 @@
 import { NextApiHandler } from "next"
-import { getToken } from "next-auth/jwt"
 
 import { config } from "config"
-import { HttpClient } from "lib/HttpClient"
 import { HttpError } from "lib/HttpError"
+import { request } from "lib/net/request"
 
 const CommunitiesHandler: NextApiHandler = async (req, res) => {
   if (req.method !== "GET") {
     return res.status(405).end()
   }
 
-  const token = await getToken({ req })
-
-  if (!token) {
-    return res.status(401).end()
-  }
-
-  const authorization = `Bearer ${token.accessToken}`
-
   try {
-    const allComunities = await HttpClient.get<Collibra.PagedCommunityResponse>(
-      `${config.COLLIBRA_API_URL}/communities`,
-      {
-        headers: { authorization },
-        query: {
-          name: "equinor",
-          nameMatchMode: "ANYWHERE",
-        },
-      }
-    )
+    const communitiesParams = new URLSearchParams({ name: "equinor", nameMatchMode: "ANYWHERE" }).toString()
 
-    const equinorCommunity = allComunities.body?.results[0]
+    const allCommunitiesRes = await request(`${config.COLLIBRA_API_URL}/communities?${communitiesParams}`, {
+      retries: 3,
+    })({ req })
+    const allComunities: Collibra.PagedCommunityResponse = await allCommunitiesRes.json()
+
+    const equinorCommunity = allComunities.results[0]
 
     if (!equinorCommunity) {
       return res.status(500).end()
     }
 
-    const communities = await HttpClient.get<Collibra.PagedCommunityResponse>(
-      `${config.COLLIBRA_API_URL}/communities`,
-      {
-        headers: { authorization },
-        query: { parentId: equinorCommunity.id },
-      }
-    )
+    const communitiesRes = await request(`${config.COLLIBRA_API_URL}/communities?parentId=${equinorCommunity.id}`)({
+      req,
+    })
+    const communities: Collibra.PagedCommunityResponse = await communitiesRes.json()
 
-    return res.json(communities.body?.results)
+    return res.json(communities.results)
   } catch (error) {
     /* eslint-disable no-console  */
     console.log("[CommunitiesHandler]", error)
