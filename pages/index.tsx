c730f43e7da6104fa92bc3ba690e35da02173d87
Shikha Mishra <shikhamishra.jec@gmail.com>
2023-02-28 11:24:02 +0100

    Add relevant Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
 /* eslint-disable camelcase */
-import { Icon } from "@equinor/eds-core-react"
+import { Icon, Typography, Card } from "@equinor/eds-core-react"
 import { info_circle, search } from "@equinor/eds-icons"
 import { tokens } from "@equinor/eds-tokens"
 import type { NextPage, GetServerSideProps } from "next"
 import NextLink from "next/link"
-import { useIntl } from "react-intl"
+import { FormattedMessage, useIntl } from "react-intl"
 import styled from "styled-components"
 
 import { Banner } from "components/Banner"
 import { Page } from "components/Page"
 import { Section } from "components/Section"
 import { Heading } from "components/Typography"
+
+const CardGrid = styled(Card)`
+  justify-content: space-between;
+  height: 100%;
+`
+
+const SectionHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  margin-bottom: 1.5rem;
+`
 
 const SearchButtonContainer = styled.div`
   margin-bottom: 3rem;
@@ -37,6 +49,16 @@ const SearchButton = styled(NextLink)`
   &:focus-visible {
     outline: 2px dashed ${tokens.colors.interactive.primary__resting.hex};
   }
+`
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(min(100%, 28ch), 1fr));
+  grid-gap: 1.5rem;
+`
+
+const StyledLink = styled(NextLink)`
+  text-decoration: none;
 `
 
 const Hero = styled.div`
@@ -88,6 +110,72 @@ const Frontpage: NextPage<Props> = ({ featureFlags = { USE_IMPROVED_SEARCH: fals
               </Banner>
             </HeroContent>
           </Hero>
+        </Section>
+
+        <Section highlight>
+          <SectionHeader>
+            <Typography variant="h3">
+              <FormattedMessage id="frontpage.relevantdata.header" />
+            </Typography>
+          </SectionHeader>
+          <GridContainer>
+            <StyledLink href={{ pathname: "https://statoilsrm.sharepoint.com/sites/Datatomany" }} target="_blank">
+              <CardGrid elevation="raised">
+                <Card.Header>
+                  <Card.HeaderTitle>
+                    <Typography variant="h5">
+                      <FormattedMessage id="frontpage.relevantdata.data.tomany" />
+                    </Typography>
+                  </Card.HeaderTitle>
+                </Card.Header>
+                <Card.Content>
+                  <Typography variant="body_short">
+                    <FormattedMessage id="frontpage.relevantdata.data.tomany.text" />
+                  </Typography>
+                </Card.Content>
+              </CardGrid>
+            </StyledLink>
+            <StyledLink
+              href={{ pathname: "https://statoilsrm.sharepoint.com/sites/Dataanalyticsnetwork" }}
+              target="_blank"
+            >
+              <CardGrid elevation="raised">
+                <Card.Header>
+                  <Card.HeaderTitle>
+                    <Typography variant="h5">
+                      <FormattedMessage id="frontpage.relevantdata.data.analytics" />
+                    </Typography>
+                  </Card.HeaderTitle>
+                </Card.Header>
+                <Card.Content>
+                  <Typography variant="body_short">
+                    <FormattedMessage id="frontpage.relevantdata.data.analytics.text" />
+                  </Typography>
+                </Card.Content>
+              </CardGrid>
+            </StyledLink>
+            <StyledLink
+              href={{
+                pathname: "https://statoilsrm.sharepoint.com/sites/EDM/SitePages/Data%20Product%20Playbook.aspx",
+              }}
+              target="_blank"
+            >
+              <CardGrid elevation="raised">
+                <Card.Header>
+                  <Card.HeaderTitle>
+                    <Typography variant="h5">
+                      <FormattedMessage id="frontpage.relevantdata.data.product" />
+                    </Typography>
+                  </Card.HeaderTitle>
+                </Card.Header>
+                <Card.Content>
+                  <Typography variant="body_short">
+                    <FormattedMessage id="frontpage.relevantdata.data.product.text" />
+                  </Typography>
+                </Card.Content>
+              </CardGrid>
+            </StyledLink>
+          </GridContainer>
         </Section>
       </main>
     </Page>
