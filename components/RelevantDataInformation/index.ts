700d6db37944217449ff7cc0f6b5c2d9d59e92c3
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-03-02 11:10:00 +0100

    🔀 Rebase from main

--- components/RelevantDataInformation/index.ts
+++ components/RelevantDataInformation/index.ts
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 export { RelevantDataInformation } from "./RelevantDataInformation"
-=======
-export { DataInformationCard } from "./DataInformationCard"
->>>>>>> 3bdf17a (♻️ Create a component for the information card)
-=======
-export { RelevantDataInformation } from "./RelevantDataInformation"
->>>>>>> 68caf29 (♻️ Refactor relevant data to a component)
