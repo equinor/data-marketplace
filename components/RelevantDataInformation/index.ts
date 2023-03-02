418d8e547ae73beac31a444006e34fac9e596775
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-03-02 09:37:32 +0100

    ♻️ Create a component for the information card

--- components/RelevantDataInformation/index.ts
+++ components/RelevantDataInformation/index.ts
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export { RelevantDataInformation } from "./RelevantDataInformation"
+=======
+export { DataInformationCard } from "./DataInformationCard"
+>>>>>>> 22ad386 (♻️ Create a component for the information card)
