72490bb6565bed514d6ffcc5dc3ff1522652a65b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-03-02 09:52:32 +0100

    ♻️ Refactor relevant data to a component

--- components/RelevantDataInformation/index.ts
+++ components/RelevantDataInformation/index.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export { RelevantDataInformation } from "./RelevantDataInformation"
-=======
-export { DataInformationCard } from "./DataInformationCard"
->>>>>>> 22ad386 (♻️ Create a component for the information card)
