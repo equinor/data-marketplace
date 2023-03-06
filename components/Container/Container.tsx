6e38a5f7735170cb14ade41ef5a6163c6bb4d799
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-03-06 09:35:43 +0100

    🔥 Remove unused component

--- components/Container/Container.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-export const Container = styled.div`
-  width: 100%;
-  margin: 0 auto;
-  padding-inline: var(--layout-padding-inline);
-  max-width: var(--layout-max-width);
-`
