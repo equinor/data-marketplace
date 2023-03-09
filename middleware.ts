2344dbbf5a99e28d2f055936a93146f21ee7eb7d
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-03-09 08:29:57 +0100

    🔧 Remove search-beta from auth config

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-restricted-exports, import/no-default-export
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/", "/assets/:path*", "/checkout/:path*", "/search", "/search-beta"] }
+export const config = { matcher: ["/", "/assets/:path*", "/checkout/:path*", "/search"] }
