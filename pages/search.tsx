295fc61c425b63f37cf9595e25f7647a2ba76393
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-03-09 08:25:57 +0100

    🔥 Remove the old search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,131 +1,197 @@
-import { Divider, Typography, CircularProgress, List, Pagination } from "@equinor/eds-core-react"
+// eslint-disable-next-line import/extensions
 import { tokens } from "@equinor/eds-tokens"
-import type { NextPage } from "next"
-import { useRouter } from "next/router"
-import React, { useMemo, MouseEvent, KeyboardEvent } from "react"
-import { FormattedMessage, useIntl } from "react-intl"
+import { createInstantSearchNextRouter } from "instantsearch-router-next-experimental"
+import type { NextPage, GetServerSideProps } from "next/types"
+import { renderToString } from "react-dom/server"
+import { getServerState } from "react-instantsearch-hooks-server"
+import {
+  Configure,
+  InstantSearch,
+  InstantSearchServerState,
+  InstantSearchSSRProvider,
+} from "react-instantsearch-hooks-web"
+import { FormattedMessage, useIntl, IntlProvider } from "react-intl"
 import styled from "styled-components"
 
 import { Container } from "components/Container"
+import { SearchBox, Hits, Hit, RefinementList, Pagination, CustomClearRefinement } from "components/ImprovedSearch"
 import { Page } from "components/Page"
-import { SearchResultStats, Filter, Hit } from "components/Search"
-import { useSearchResults } from "hooks"
+import { SearchStatistics } from "components/SearchStatistics"
+import { Heading } from "components/Typography"
+import { searchClient, searchClientServer } from "config"
+import englishTexts from "locales/english.json"
 
-const SearchPageContainer = styled.div`
-  margin-top: ${tokens.spacings.comfortable.x_large};
+const SearchContainer = styled.div`
+  --huge-space: calc(2 * ${tokens.spacings.comfortable.xxx_large});
+  /* Lot's of different text sizes going on here, I expect this to change
+  at some point. But add an explicit height for now and reuse as line height for 
+  the stats header to get proper alignment */
+  --baseline-text-height: 2.25rem;
   display: grid;
-  grid-template-columns: 15rem 1fr;
-  grid-gap: ${tokens.spacings.comfortable.xx_large};
+  grid-template-areas:
+    "search"
+    "filter"
+    "totalResults"
+    "results"
+    "pagination";
+  @media (min-width: 900px) {
+    /* Temp. column width restriction until new page layout is ready */
+    grid-template-columns: 17rem var(--huge-space) minmax(auto, 660px);
+    grid-template-rows: min-content var(--huge-space) var(--baseline-text-height) auto auto;
+    grid-template-areas:
+      ". . search"
+      ". . ."
+      "filterHeader . totalResults"
+      "filter .  results"
+      ". . pagination";
+  }
 `
 
-const SearchResultsHeader = styled(Typography)`
-  margin-bottom: ${tokens.spacings.comfortable.medium};
-  /* Same line height as the filter header, pixel perfect ftw */
-  line-height: 1.6em;
+const StyledPagination = styled(Pagination)`
+  justify-content: center;
 `
 
-const UnstyledList = styled(List)`
-  list-style: none;
-  padding: 0;
+const StyledSearchBox = styled.div`
+  grid-area: search;
 `
 
-const FieldSetStyle = styled.fieldset`
-  border: 0;
-  padding: 0;
+const FilterHeader = styled.div`
+  grid-area: filterHeader;
 `
-const LegendC = styled.legend`
-  margin-bottom: ${tokens.spacings.comfortable.small};
+
+const Filters = styled.div`
+  grid-area: filter;
+  border-top: 1px solid ${tokens.colors.text.static_icons__tertiary.hsla};
 `
-/* Needs a wrapper because if we use the Pagination Component, the class is added to the
-pagination nav, not the container containing the with item results component */
+
 const PaginationContainer = styled.div`
-  margin-top: ${tokens.spacings.comfortable.large};
+  grid-area: pagination;
 `
 
-const HITS_PER_PAGE = 20
+const StyledHits = styled.div`
+  grid-area: results;
+`
 
-const Search: NextPage = () => {
-  const { searchResults = [], total, isLoading, error: searchResultError } = useSearchResults()
-  const router = useRouter()
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: baseline;
+`
+
+const TotalResults = styled.div`
+  grid-area: totalResults;
+  display: flex;
+  align-items: baseline;
+`
+
+type Props = {
+  serverState?: InstantSearchServerState
+  serverUrl?: URL | string
+  isServerRendered: boolean
+  indexName: string
+}
+
+const HITS_PER_PAGE = 10
+
+// Because there so many thing going on without this
+const onStateChange = (params: any) => {
+  params.setUiState(params.uiState)
+}
+
+const Search = ({ serverState, isServerRendered, indexName, serverUrl }: Props) => (
+  /* eslint-disable-next-line react/jsx-props-no-spreading */
+  <InstantSearchSSRProvider {...serverState}>
+    <IntlProvider locale="en" defaultLocale="en" messages={englishTexts}>
+      <InstantSearch
+        searchClient={isServerRendered ? searchClientServer : searchClient}
+        indexName={indexName}
+        onStateChange={onStateChange}
+        /* @ts-ignore */
+        routing={{ router: createInstantSearchNextRouter({ serverUrl }) }}
+      >
+        <Configure
+          hitsPerPage={HITS_PER_PAGE}
+          snippetEllipsisText="..."
+          attributesToSnippet={["excerpt:30", "description:30"]}
+        />
+
+        <SearchContainer>
+          <StyledSearchBox>
+            <SearchBox />
+          </StyledSearchBox>
+          <TotalResults>
+            <SearchStatistics style={{ lineHeight: "var(--baseline-text-height)" }} hitsPerPage={HITS_PER_PAGE} />
+          </TotalResults>
+          <StyledHits>
+            {/* @ts-ignore  */}
+            <Hits hitComponent={Hit} />
+          </StyledHits>
+          <FilterHeader>
+            <Header>
+              <Heading level="h3" size="lg">
+                <FormattedMessage id="improvedSearch.filter.header" />
+              </Heading>
+              <CustomClearRefinement />
+            </Header>
+          </FilterHeader>
+          <Filters>
+            <RefinementList attribute="community">
+              <FormattedMessage id="improvedSearch.community.filter.header" />
+            </RefinementList>
+          </Filters>
+
+          <PaginationContainer>
+            <StyledPagination hitsPerPage={HITS_PER_PAGE} padding={1} />
+          </PaginationContainer>
+        </SearchContainer>
+      </InstantSearch>
+    </IntlProvider>
+  </InstantSearchSSRProvider>
+)
+
+const SearchPage: NextPage<Props> = ({ serverState, isServerRendered = false, serverUrl, indexName }) => {
   const intl = useIntl()
-
-  const onPaginationChange = (event: MouseEvent | KeyboardEvent | null, page: number) => {
-    const offset = page - 1
-    router.replace({ query: { ...router.query, offset } }, { query: { ...router.query, offset } }, { shallow: true })
-  }
-
-  const numberOfFilters = useMemo(() => {
-    const appliedFilters = router.query.community
-    if (!appliedFilters) return 0
-    if (typeof appliedFilters === "string") {
-      return 1
-    }
-    return appliedFilters.length
-  }, [router.query.community])
-
-  if (searchResultError) {
-    /* eslint-disable no-console */
-    console.log("[Search] Failed fetching search results", searchResultError)
-  }
-
-  const searchQuery = router.query.q
   return (
     <Page documentTitle={intl.formatMessage({ id: "search.title" })}>
       <main>
-        <Container>
-          <Typography variant="h1">{intl.formatMessage({ id: "search.header" })}</Typography>
-          <SearchPageContainer>
-            <aside>
-              <Typography variant="h4" as="h2">
-                <FormattedMessage id="search.filterHeader" />
-              </Typography>
-              <Divider variant="small" />
-              <FieldSetStyle>
-                <LegendC>
-                  <FormattedMessage id="search.communitiesHeader" />
-                </LegendC>
-                <Filter disabled={total === 0} />
-              </FieldSetStyle>
-            </aside>
-            <div>
-              {isLoading ? (
-                <CircularProgress />
-              ) : (
-                <>
-                  <SearchResultsHeader variant="body_short">
-                    {!searchQuery || searchQuery === "" ? (
-                      <FormattedMessage id="search.no.query" />
-                    ) : (
-                      <SearchResultStats numberOfHits={total} numberOfFilters={numberOfFilters} query={searchQuery} />
-                    )}
-                  </SearchResultsHeader>
-
-                  {searchResults.length > 0 && (
-                    <UnstyledList variant="numbered">
-                      {searchResults.map((resource) => (
-                        <Hit key={resource.id} hit={resource} />
-                      ))}
-                    </UnstyledList>
-                  )}
-                </>
-              )}
-              {total !== null && total > HITS_PER_PAGE && (
-                <PaginationContainer>
-                  <Pagination
-                    itemsPerPage={HITS_PER_PAGE}
-                    onChange={onPaginationChange}
-                    totalItems={total}
-                    withItemIndicator
-                    defaultPage={Number.isNaN(Number(router.query.offset)) ? 1 : Number(router.query.offset) + 1}
-                  />
-                </PaginationContainer>
-              )}
-            </div>
-          </SearchPageContainer>
+        <Container highlight>
+          <Heading level="h1" size="2xl" center style={{ marginBlock: tokens.spacings.comfortable.xxx_large }}>
+            <FormattedMessage id="improvedSearch.header" />
+          </Heading>
+          <Search
+            serverState={serverState}
+            indexName={indexName}
+            isServerRendered={isServerRendered}
+            serverUrl={serverUrl}
+          />
         </Container>
       </main>
     </Page>
   )
 }
 
-export default Search
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const indexName = process.env.ALGOLIA_SEARCH_INDEX ?? ""
+
+  if (indexName === "") {
+    console.log("Missing the Algolia search index name")
+  }
+
+  const protocol = req.headers.referer?.split("://")[0] || "https"
+  const serverUrl = `${protocol}://${req.headers.host}${req.url}`
+
+  const serverState = await getServerState(<Search serverUrl={serverUrl} isServerRendered indexName={indexName} />, {
+    renderToString,
+  })
+
+  return {
+    props: {
+      serverState,
+      serverUrl,
+      indexName,
+    },
+  }
+}
+
+export default SearchPage
