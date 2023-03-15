690a54225d192ccbda7339e8fb23ec398ccf4efb
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-03-15 11:32:19 +0100

    ✨ Add new metadata feature
    We can't use hooks here so just did it like this

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,16 @@
+import type { Metadata } from "next"
 import { notFound } from "next/navigation"
 
 import { FrontPage } from "./FrontPage"
 
+import englishTexts from "locales/english.json"
+
+export async function generateMetadata(): Promise<Metadata> {
+  const title: string = englishTexts["common.documentTitle"]
+  return { title }
+}
+
+// eslint-disable-next-line import/no-default-export
 export default async function Page() {
   const USE_APP_FOLDER = process.env.USE_APP_FOLDER === "true"
   const indexName = process.env.ALGOLIA_SEARCH_INDEX ?? ""
