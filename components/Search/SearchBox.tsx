ac2c37c52029c20d5fe3524a2fe08df94f416cfd
Shikha Mishra <shikhamishra.jec@gmail.com>
2023-03-17 07:50:40 +0100

    Changed search box

--- components/Search/SearchBox.tsx
+++ components/Search/SearchBox.tsx
@@ -1,12 +1,22 @@
-import { Search } from "@equinor/eds-core-react"
-import { tokens } from "@equinor/eds-tokens"
 import { useState, useRef /* useEffect */ } from "react"
 import { useSearchBox } from "react-instantsearch-hooks-web"
 import { useIntl } from "react-intl"
 import styled from "styled-components"
 
-const StyledSearch = styled(Search)`
-  --eds_ui_background__light: ${tokens.colors.ui.background__default.hsla};
+const Input = styled.input`
+  background-color: var(--white);
+  border: 2px solid var(--moss-green-100);
+  padding: 2rem;
+  font-size: var(--typeScale-1);
+  width: 100%;
+  color: black;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 1px var(--white);
+  }
+
+  }
 `
 
 export const SearchBox = () => {
@@ -39,11 +49,21 @@ export const SearchBox = () => {
   }
 
   return (
-    <StyledSearch
-      ref={inputRef}
-      onChange={handleOnChange}
-      value={inputValue}
-      placeholder={intl.formatMessage({ id: "search.box.placeholder" })}
-    />
+    <form action="" role="search" noValidate>
+      <Input
+        id="site-search"
+        ref={inputRef}
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
+        spellCheck={false}
+        maxLength={512}
+        type="search"
+        value={inputValue}
+        onChange={handleOnChange}
+        placeholder={intl.formatMessage({ id: "search.box.placeholder" })}
+        /*      onInput={handleReset} */
+      />
+    </form>
   )
 }
