e9bf0dfb064b02bb572f31677f17e61dcbdad31f
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-04-03 14:51:27 +0200

    💚 Yet another try for CI (ab#105193)

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -90,9 +90,7 @@ Cypress.Commands.add("mockLogin", () => {
     // We are currently unsure about this part.
     // We need to refresh this cookie once in a while.
     // We are unsure if this is true and if true, when it needs to be refreshed.
-    cy.setCookie("next-auth.session-token", Cypress.env("NEXT_AUTH_SESSION_TOKEN"), {
-      secure: true,
-    })
+    cy.setCookie("next-auth.session-token", Cypress.env("NEXT_AUTH_SESSION_TOKEN"))
   })
   // Cypress.Cookies.preserveOnce("next-auth.session-token")
 })
