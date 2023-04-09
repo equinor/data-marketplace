72a4f5cfffceff0ccc8d7633624ff0b0896ec109
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-09 12:20:11 +0800

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-restricted-exports, import/no-default-export
-export { default } from "next-auth/middleware"
-
-export const config = { matcher: ["/", "/assets/:path*", "/checkout/:path*", "/search", "/test"] }
